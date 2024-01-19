67f376cbb8a1e42671758bb8f05fe0769dc878b5
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-19 10:55:53 +0800

    Import icons from material ui

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,11 @@
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+
 export default function TopBar() {
     return (
         <nav className="flex justify-between items-center p-1 md:hidden">
             <button className="p-4">
-                <NotificationsIcon />
+                <NotificationsOutlinedIcon />
             </button>
 
             <Link className="" href="/">
