404b1ce484e85385c9b7b73d4c2c3926020bfba9
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-01-19 08:54:52 +0800

    Add TopBar component to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import TopBar from "@/components/TopBar";
 
 const sora = Sora({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={`${sora.className} bg-primary-neutral text-primary-white`}>
+            <TopBar />
             {children}
          </body>
       </html>
