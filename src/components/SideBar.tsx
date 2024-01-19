dfafcda696ae088e03a27fd5042226f893d8d28a
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-19 11:28:48 +0800

    Add logo, notification, my account, and log out in sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,21 @@
 export default function SideBar() {
-    return (
-        <nav></nav>
-    );
+  return (
+    <nav className="hidden h-screen w-60 flex-col items-center p-4 md:flex">
+      <Link className="" href="/">
+        <p>LOGO</p>
+      </Link>
+      <button className="w-full p-2">
+        <NotificationsOutlinedIcon />
+        <p>Notification</p>
+      </button>
+      <button className="w-full p-2">
+        <PermIdentityOutlinedIcon />
+        <p>My Account</p>
+      </button>
+      <button className="w-full p-2">
+        <LogoutOutlinedIcon />
+        <p>Log out</p>
+      </button>
+    </nav>
+  );
 }
