f662f533bf1b3ba25407fc7e91af79b8a402d7f2
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-01-19 09:15:22 +0800

    Add notification, logo, and menu in TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,6 @@
-'use client'
 import Link from 'next/link';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
-import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
-import { useState } from 'react';
 
 export default function TopBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -26,38 +21,18 @@ export default function TopBar() {
     }
 
     return (
-        <nav className="sticky top-0 flex justify-between items-center bg-primary-black md:hidden">
-            <div>
-                <button onClick={onClickToggleNotification} className="p-4">
-                    <NotificationsOutlinedIcon />
-                </button>
-                <div className={`absolute top-full left-0 bg-primary-neutral w-full p-2 ${isNotificationOpen ? 'scale-100' : 'scale-0'} `}><h1>No Notifications</h1></div>
-            </div>
+        <nav className="flex justify-between items-center p-1">
+            <button className="p-4">
+                <NotificationsOutlinedIcon />
+            </button>
 
             <Link className="" href="/">
                 <p>LOGO</p>
             </Link>
 
-            <div>
-                <button onClick={onClickOpenMenu} className="p-4">
-                    <MenuIcon />
-                </button>
-                    <div className={`bg-primary-black h-screen w-3/4 absolute top-0 flex flex-col justify-end p-2 ${isMenuOpen ? 'right-0' : 'right-full' }`}>
-                        <button onClick={onClickCloseMenu} className='absolute top-0 right-full p-4'>
-                            <CloseIcon />
-                        </button>
-                        <div className="flex flex-col gap-2 w-full">
-                            <button className="w-full p-2 flex gap-2">
-                                <PermIdentityOutlinedIcon />
-                                <p>My Account</p>
-                            </button>
-                            <button className="w-full p-2 flex gap-2">
-                                <LogoutOutlinedIcon />
-                                <p>Log out</p>
-                            </button>
-                        </div>
-                    </div>
-            </div>
+            <button className="p-4">
+                <MenuIcon />
+            </button>
         </nav>
     );
 }
