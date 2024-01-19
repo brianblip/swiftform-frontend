ad1dc99c93164d71348fc099f53e4f5c6fbd48bb
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-19 12:47:02 +0800

    remove p-1 in nav

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,7 +7,7 @@ import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 export default function TopBar() {
     return (
-        <nav className="flex justify-between items-center p-1 md:hidden">
+        <nav className="flex justify-between items-center md:hidden">
             <button className="p-4">
                 <NotificationsOutlinedIcon />
             </button>
