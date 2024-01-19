2cd470680741b5d6e3755ed3a78791fc0bff2cd6
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-19 12:44:34 +0800

    Add close button, my account, and logout in menu panel

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 export default function TopBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -35,6 +38,17 @@ export default function TopBar() {
                     <MenuIcon />
                 </button>
                     <div className='bg-primary-black h-screen w-3/4 absolute right-0 top-0'>
+                        <button className='absolute top-0 right-full p-4'>
+                            <CloseIcon />
+                        </button>
+                        <button className="w-full p-2">
+                            <PermIdentityOutlinedIcon />
+                            <p>My Account</p>
+                        </button>
+                        <button className="w-full p-2">
+                            <LogoutOutlinedIcon />
+                            <p>Log out</p>
+                        </button>
                     </div>
             </div>
         </nav>
