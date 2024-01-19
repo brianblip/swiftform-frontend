9715d93c85c26f801eb933cd388c17b93eab0c28
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-01-19 09:16:25 +0800

    Hide TopBar component on md breakpoint

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -4,7 +4,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 export default function TopBar() {
     return (
-        <nav className="flex justify-between items-center p-1">
+        <nav className="flex justify-between items-center p-1 md:hidden">
             <button className="p-4">
                 <NotificationsOutlinedIcon />
             </button>
