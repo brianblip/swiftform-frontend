0f8e7ce67225bf86aad18cba63482428cde6cf02
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-01-19 09:15:22 +0800

    Add notification, logo, and menu in TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+
 export default function TopBar() {
     return (
-        <nav></nav>
+        <nav className="flex justify-between items-center p-1">
+            <button className="p-4">
+                <NotificationsOutlinedIcon />
+            </button>
+
+            <Link className="" href="/">
+                <p>LOGO</p>
+            </Link>
+
+            <button className="p-4">
+                <MenuIcon />
+            </button>
+        </nav>
     );
 }
