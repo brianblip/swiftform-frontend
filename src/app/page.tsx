a9b05483c03a3b27a3be27d5ee1496433da0468f
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-22 11:03:31 +0800

    add styling to parent container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <main className="h-[calc(100dvh-57.0667px)] w-dvw p-4 grid place-items-center">
          <div className="w-full flex flex-col items-center gap-y-4">
             <h1 className="text-3xl font-bold">Forms</h1>
-            <div>
+            <div className="w-full flex flex-col items-center gap-y-6">
                <div className="w-11/12 grid grid-cols-2 gap-x-2 gap-y-4">
                   <SuggestionButton />
                   <SuggestionButton />
