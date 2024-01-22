42ea36069577dd028042b31b9d53479211295b09
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-22 08:38:55 +0800

    create state variable for profile panel on sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 
 export default function SideBar() {
   const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
 
   /* function for toggling the notification panel */
   function onClickToggleNotification() {
