e15d0f812c2b5cc6ef16075947558898e7705f1c
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-22 11:03:04 +0800

    create parent div for container of suggestionButton and 'create new form' button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
       <main className="h-[calc(100dvh-57.0667px)] w-dvw p-4 grid place-items-center">
          <div className="w-full flex flex-col items-center gap-y-4">
             <h1 className="text-3xl font-bold">Forms</h1>
+            <div>
                <div className="w-11/12 grid grid-cols-2 gap-x-2 gap-y-4">
                   <SuggestionButton />
                   <SuggestionButton />
@@ -12,6 +13,7 @@ export default function Home() {
                   <SuggestionButton />
                </div>
                <button className="w-full bg-primary-white text-primary-black px-2 py-3">Create new form</button>
+            </div>
          </div>
       </main>
    );
