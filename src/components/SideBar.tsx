f22eb37a37f62bacf0641028dcb8a2f7d7697b94
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-22 08:41:45 +0800

    move notification panel outside of notification button container

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -29,8 +29,9 @@ export default function SideBar() {
           <button onClick={onClickToggleNotification} className="w-full p-2 flex gap-2">
             <NotificationsOutlinedIcon />
             <p>Notification</p>
-            <div className={`absolute bottom-full left-0 bg-primary-neutral w-full p-2 ${isNotificationOpen ? 'scale-100' : 'scale-0'} `}><h1>No Notifications</h1></div>
           </button>
+          <div className={`absolute bottom-full left-0 bg-primary-neutral w-full p-2 ${isNotificationOpen ? 'scale-100' : 'scale-0'} `}><h1>No Notifications</h1></div>
+        </div>
         </div>
         <button className="w-full p-2 flex gap-2">
           <PermIdentityOutlinedIcon />
