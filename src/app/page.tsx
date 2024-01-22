b23bcaeac6918b876029a25a3db977de67b889ee
Sly Villafania <villafania.sly.lau@gmail.com>
2024-01-22 10:57:10 +0800

    add styling to previous elements and create parent container for suggestionButtons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import SuggestionButton from "@/components/SuggestionButton";
 
 export default function Home() {
    return (
-      <main>
-         <div>
-            <h1>Forms</h1>
+      <main className="h-[calc(100dvh-57.0667px)] w-dvw p-4 grid place-items-center">
+         <div className="w-full flex flex-col items-center gap-y-4">
+            <h1 className="text-3xl font-bold">Forms</h1>
+               <div>
                   <SuggestionButton />
+                  <SuggestionButton />
+                  <SuggestionButton />
+                  <SuggestionButton />
+               </div>
          </div>
       </main>
    );
