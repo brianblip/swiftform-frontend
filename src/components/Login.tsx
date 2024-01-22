dd406d15c064516e92aa3bbe3f6de21ee29e154b
Neo <lordneobarnachea@gmail.com>
2024-01-22 14:33:47 +0800

    add forgot password

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
     return (
         <div className='h-[100vh] bg-white w-screen flex flex-col justify-center items-center min-w-[352px]'>
-            <div className='border rounded-lg border-gray-300 w-[80%] md:w-[50%] h-[80%] flex flex-col items-center gap-9 justify-center max-h-[1300px] '>
+            <div className='border rounded-lg border-gray-300 w-[80%] md:w-[50%] h-[85%] flex flex-col items-center gap-9 justify-center max-h-[1300px] '>
                 <div className="border border-red-500 w-28 h-36 rounded-full overflow-hidden" >
                     LOGO
                 </div>
@@ -29,6 +29,7 @@ export default function Login() {
                             id='password'
                         />
                     </div>
+                    <p className='text-black cursor-pointer w-[60%] mb-4'>Forgot password?</p>
                     <button
                         type='submit'
                         className='border w-[60%] max-w-md rounded-md h-[52px] border-black py-2 px-3 bg-black text-white leading-tight '
