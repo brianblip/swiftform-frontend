1e9d4e56e86a3d7f34513a0db1653948d26207b7
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-22 20:56:24 +0800

    Create new route to handle FormCreation component

---
+++ src/app/FormCreate/page.tsx
@@ -0,0 +1,9 @@
+import DynamicForm from '@/components/DynamicForm';
+
+export default function FormCreate() {
+   return (
+      <main className="h-[calc(100dvh-57.0667px)] w-dvw p-4 grid place-items-center sm:p-8 md:h-dvh">
+         <DynamicForm />
+      </main>
+   );
+};
