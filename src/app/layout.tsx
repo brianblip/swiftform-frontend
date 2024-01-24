652e3aced9211cc5d22065528658e74d14f58948
Interns@BOOMTECH <internsatboomtech@gmail.com>
2024-01-24 04:46:01 +0000

    Merged in sly/add-navbar-v2 (pull request #13)
    Create navbar with topbar and sidebar components
    
    * add parent div with relative styling for absolute notification panel later
    
    * create notification panel
    
    * add static styling to notification panel
    
    * add state variables for dynamic styling to notification panel
    
    * Merge branch 'sly/add-navbar-v2' of https://bitbucket.org/upang-internship-2024/swiftform-frontend into sly/add-navbar-v2
    
    * create state variable for profile panel
    
    * create function for toggling the profile panel
    
    * create parent div for profile container
    
    * add function to onClick property of my account button
    
    * Add profile panel and button 'edit profile'
    
    * add styling to parent container of my account button and profile panel
    
    * add dynamic styling on profile panel and static style on edit profile button
    
    * create state variable for profile panel on sidebar
    
    * create function for toggling the profile panel on sidebar
    
    * move notification panel outside of notification button container
    
    * create parent div for profile container on sidebar
    
    * add styling to parent div on sidebar
    
    * add onclick property with toggle function on my account button
    
    * create container and button for edit profile on my account
    
    * add dynamic styling to edit proifile container
    
    
    Approved-by: Jan Tolentino

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import TopBar from "@/components/TopBar";
+import Sidebar from "@/components/SideBar";
 
 const sora = Sora({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
    return (
       <html lang="en">
-         <body className={`${sora.className} bg-primary-neutral text-primary-white`}>
+         <body className={`${sora.className} bg-primary-neutral text-primary-white md:flex`}>
+            <TopBar />
+            <Sidebar />
             {children}
          </body>
       </html>
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,74 @@
+'use client'
+import Link from 'next/link';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { useState } from 'react';
+
+export default function TopBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
+
+    /* function for opening and closing mobile menu */
+    function onClickOpenMenu() {
+        setIsMenuOpen(true);
+    };
+
+    function onClickCloseMenu() {
+        setIsMenuOpen(false);
+    }
+
+    /* function for toggling the notification panel */
+    function onClickToggleNotification() {
+        setIsNotificationOpen(!isNotificationOpen);
+    }
+
+    /* function for toggling the profile panel */
+    function onClickToggleProfile() {
+        setIsProfileOpen(!isProfileOpen);
+    }
+
+    return (
+        <nav className="sticky top-0 flex justify-between items-center bg-primary-black md:hidden">
+            <div>
+                <button onClick={onClickToggleNotification} className="p-4">
+                    <NotificationsOutlinedIcon />
+                </button>
+                <div className={`absolute top-full left-0 bg-primary-neutral w-full p-2 ${isNotificationOpen ? 'scale-100' : 'scale-0'} `}><h1>No Notifications</h1></div>
+            </div>
+
+            <Link className="" href="/">
+                <p>LOGO</p>
+            </Link>
+
+            <div>
+                <button onClick={onClickOpenMenu} className="p-4">
+                    <MenuIcon />
+                </button>
+                    <div className={`bg-primary-black h-screen w-3/4 absolute top-0 flex flex-col justify-end p-2 ${isMenuOpen ? 'right-0' : 'right-full' }`}>
+                        <button onClick={onClickCloseMenu} className='absolute top-0 right-full p-4'>
+                            <CloseIcon />
+                        </button>
+                        <div className="flex flex-col gap-2 w-full">
+                            <div className='relative'>
+                                <button onClick={onClickToggleProfile} className="w-full p-2 flex gap-2">
+                                    <PermIdentityOutlinedIcon />
+                                    <p>My Account</p>
+                                </button>
+                                    <div className={`absolute bottom-full left-0 mb-2 w-full flex-col items-start bg-primary-neutral ${isProfileOpen ? 'flex' : 'hidden' }`}>
+                                        <button className='p-2 w-full text-start'>Edit Profile</button>
+                                    </div>
+                            </div>
+                            <button className="w-full p-2 flex gap-2">
+                                <LogoutOutlinedIcon />
+                                <p>Log out</p>
+                            </button>
+                        </div>
+                    </div>
+            </div>
+        </nav>
+    );
+}
