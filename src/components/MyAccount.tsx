bcf3934b73ff5f05432da7395806f7a9d4169cb6
Neo <lordneobarnachea@gmail.com>
2024-01-24 16:52:06 +0800

    add use client

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -1,3 +1,4 @@
+'use client'
 interface InputProps {
     label: string,
     placeholder: string,
@@ -16,6 +17,34 @@ const Input = ({ label, placeholder, type }: InputProps) => {
                 placeholder={placeholder}
                 type={type}
             />
+        </div>
+    )
+}
+
+export default function MyAccount() {
+    return (
+        <div className="border border-red-500 w-full px-20 py-20">
+            <p className="text-center text-3xl">My account</p>
+
+            <div>
+                <p className="text-xl">Profile Information</p>
+                <div></div>
+                <div className="flex flex-row justify-center gap-10">
+                    <Input type="name" placeholder="First name" label="First name" />
+                    <Input type="name" placeholder="Last name" label="Last name" />
+                </div>
+                <div className="flex flex-row justify-center gap-10">
+                    <Input type="email" placeholder="Email" label="Email" />
+                </div>
+                <div></div>
+            </div>
+
+            <div>
+                <p className="text-xl">Account Setting</p>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     )
 }
