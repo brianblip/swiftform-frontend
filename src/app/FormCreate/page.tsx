8796b895e0571654af7aef582880cb425a080c93
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-25 22:52:56 +0800

    Rename FormCreate route to 'Form'; Create a Dynamic Route directory inside for 'Single Point Request'

--- src/app/FormCreate/page.tsx
+++
@@ -1,9 +0,0 @@
-import DynamicForm from '@/components/DynamicForm';
-
-export default function FormCreate() {
-   return (
-      <main className="h-[calc(100dvh-57.0667px)] w-dvw p-4 grid place-items-center sm:p-8 md:h-dvh">
-         <DynamicForm />
-      </main>
-   );
-};
