e2bacb35700ebb979c6c36acfcc1df447d4b7eff
Neo <lordneobarnachea@gmail.com>
2024-01-25 20:51:35 +0800

    Add account settings

---
+++ src/components/profile/AccountSettings.tsx
@@ -0,0 +1,11 @@
+import { Input } from "./MyAccount"
+
+export default function AccountSettings() {
+    return (
+        <div>
+            <p className="text-xl">Account Setting</p>
+            <div className="flex flex-row justify-center gap-10">
+            </div>
+        </div>
+    )
+}
