2c73fd5c9f5bf50bbad604487c9e514332709019
Neo <lordneobarnachea@gmail.com>
2024-01-25 20:49:07 +0800

    Remove Pofile Information

--- src/components/profile/MyAccount.tsx
+++ src/components/profile/MyAccount.tsx
@@ -8,7 +8,7 @@ interface InputProps {
 
 }
 
-const Input = ({ label, placeholder, type, size, id }: InputProps) => {
+export const Input = ({ label, placeholder, type, size, id }: InputProps) => {
     const getSize = () => {
         switch (size) {
             case 'sm':
@@ -38,34 +38,12 @@ const Input = ({ label, placeholder, type, size, id }: InputProps) => {
         </div>
     )
 }
-
 export default function MyAccount() {
     return (
         <div className="border border-red-500 w-full px-20 py-20 h-max">
             <p className="text-center text-3xl">My account</p>
 
-            <div>
-                <p className="text-xl">Profile Information</p>
-                <div className="h-[20vh] flex items-center gap-10 lg:pl-48">
-                    <div className="border border-red-500 rounded-full h-[120px] w-[120px]">
-
-                    </div>
-                    <button className="h-[44px] w-[176px] bg-[#444654] rounded-sm flex items-center justify-center">
-                        Change picture
-                    </button>
-                </div>
-                <div className="flex flex-row justify-center gap-10">
-                    <Input type="name" placeholder="First name" label="First name" size="md" id="firstName"/>
-                    <Input type="name" placeholder="Last name" label="Last name" size="md" id="lastName"/>
-                </div>
-                <div className="flex flex-row justify-center gap-10">
-                    <Input type="email" placeholder="Email" label="Email" size="full" id="email" />
-                </div>
-                <div className="border border-red-500 flex justify-end">
-                    <button className="h-[44px] w-[176px] bg-[#444654] rounded-sm flex items-center justify-center">Save Changes</button>
-
-                </div>
-            </div>
+            
 
             <div>
                 <p className="text-xl">Account Setting</p>
