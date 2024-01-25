e0fbac01ed62ebc4573e49399044b229a70c6cec
Neo <lordneobarnachea@gmail.com>
2024-01-25 20:50:31 +0800

    Move profile information

---
+++ src/components/profile/ProfileInformation.tsx
@@ -0,0 +1,28 @@
+import { Input } from "./MyAccount";
+
+export default function ProfileInformation() {
+    return (
+        <div>
+            <p className="text-xl">Profile Information</p>
+            <div className="h-[20vh] flex items-center gap-10 lg:pl-48">
+                <div className="border border-red-500 rounded-full h-[120px] w-[120px]">
+
+                </div>
+                <button className="h-[44px] w-[176px] bg-[#444654] rounded-sm flex items-center justify-center">
+                    Change picture
+                </button>
+            </div>
+            <div className="flex flex-row justify-center gap-10">
+                <Input type="name" placeholder="First name" label="First name" size="md" id="firstName" />
+                <Input type="name" placeholder="Last name" label="Last name" size="md" id="lastName" />
+            </div>
+            <div className="flex flex-row justify-center gap-10">
+                <Input type="email" placeholder="Email" label="Email" size="full" id="email" />
+            </div>
+            <div className="border border-red-500 flex justify-end">
+                <button className="h-[44px] w-[176px] bg-[#444654] rounded-sm flex items-center justify-center">Save Changes</button>
+
+            </div>
+        </div>
+    )
+}
