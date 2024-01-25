9253866244e115e662f7428b7957af13a36ad063
Neo <lordneobarnachea@gmail.com>
2024-01-25 20:27:28 +0800

    add id in Input component

--- src/components/profile/MyAccount.tsx
+++ src/components/profile/MyAccount.tsx
@@ -3,10 +3,12 @@ interface InputProps {
     label: string,
     placeholder: string,
     type: string,
-    size: string
+    size: string,
+    id: string
+
 }
 
-const Input = ({ label, placeholder, type, size }: InputProps) => {
+const Input = ({ label, placeholder, type, size, id }: InputProps) => {
     const getSize = () => {
         switch (size) {
             case 'sm':
@@ -24,12 +26,12 @@ const Input = ({ label, placeholder, type, size }: InputProps) => {
     return (
 
         <div className={`mb-4 ${getSize()} max-w-md`}>
-            <label className="block text-sm font-bold text-[#ECECF1]" htmlFor="email">
+            <label className="block text-sm font-bold text-[#ECECF1]" htmlFor={id}>
                 {label}
             </label>
             <input
                 className="border h-[52px] bg-[#444654] border-none w-full py-2 px-3 text-white leading-tight rounded-[4px]  focus:shadow-outline"
-                id="email"
+                id={id}
                 placeholder={placeholder}
                 type={type}
             />
@@ -39,34 +41,36 @@ const Input = ({ label, placeholder, type, size }: InputProps) => {
 
 export default function MyAccount() {
     return (
-        <div className="border border-red-500 w-full px-20 py-20">
+        <div className="border border-red-500 w-full px-20 py-20 h-max">
             <p className="text-center text-3xl">My account</p>
 
             <div>
                 <p className="text-xl">Profile Information</p>
-                <div className="h-[20vh] flex items-center gap-10 lg:pl-48  ">
+                <div className="h-[20vh] flex items-center gap-10 lg:pl-48">
                     <div className="border border-red-500 rounded-full h-[120px] w-[120px]">
 
                     </div>
-                    <div className="h-[44px] w-[176px] bg-[#444654] rounded-sm flex items-center justify-center">
-                        <button>Change picture</button>
-                    </div>
+                    <button className="h-[44px] w-[176px] bg-[#444654] rounded-sm flex items-center justify-center">
+                        Change picture
+                    </button>
                 </div>
                 <div className="flex flex-row justify-center gap-10">
-                    <Input type="name" placeholder="First name" label="First name" size="md"/>
-                    <Input type="name" placeholder="Last name" label="Last name" size="md"/>
+                    <Input type="name" placeholder="First name" label="First name" size="md" id="firstName"/>
+                    <Input type="name" placeholder="Last name" label="Last name" size="md" id="lastName"/>
                 </div>
                 <div className="flex flex-row justify-center gap-10">
-                    <Input type="email" placeholder="Email" label="Email" size="full" />
+                    <Input type="email" placeholder="Email" label="Email" size="full" id="email" />
                 </div>
-                <div></div>
+                <div className="border border-red-500 flex justify-end">
+                    <button className="h-[44px] w-[176px] bg-[#444654] rounded-sm flex items-center justify-center">Save Changes</button>
+
+                </div>
             </div>
 
             <div>
                 <p className="text-xl">Account Setting</p>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="flex flex-row justify-center gap-10">
+                </div>
             </div>
         </div>
     )
