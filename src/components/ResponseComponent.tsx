1850d8b898810597e6fe25f8bb9c9cfa09e49226
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-25 23:03:52 +0800

    Create Placeholder components for later utilization in FormBuilder;

---
+++ src/components/ResponseComponent.tsx
@@ -0,0 +1,12 @@
+// ResponseComponent.tsx
+import React from "react";
+
+export default function ResponseComponent(){
+   return (
+      <div>
+         <h2>Response Section</h2>
+      </div>
+   );
+};
+
+
