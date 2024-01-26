14367be1da261e091644d2ff27e774ecde2a8d58
Neo <lordneobarnachea@gmail.com>
2024-01-26 18:32:04 +0800

    Remove Input component

--- src/components/profile/MyAccount.tsx
+++ src/components/profile/MyAccount.tsx
@@ -1,45 +1,8 @@
 'use client'
 import ProfileInformation from "./ProfileInformation"
 import AccountSettings from "./AccountSettings"
-interface InputProps {
-    label: string,
-    placeholder: string,
-    type: string,
-    size: string,
-    id: string
 
-}
 
-export const Input = ({ label, placeholder, type, size, id }: InputProps) => {
-    const getSize = () => {
-        switch (size) {
-            case 'sm':
-                return 'w-24';
-            case 'md':
-                return 'w-[205px]';
-            case 'lg':
-                return 'w-60';
-            case 'full':
-                return 'w-full';
-            default:
-                return 'w-48';
-        }
-    };
-    return (
-
-        <div className={`mb-4 ${getSize()} max-w-md`}>
-            <label className="block text-sm font-bold text-[#ECECF1]" htmlFor={id}>
-                {label}
-            </label>
-            <input
-                className="border h-[52px] bg-[#444654] border-none w-full py-2 px-3 text-white leading-tight rounded-[4px]  focus:shadow-outline"
-                id={id}
-                placeholder={placeholder}
-                type={type}
-            />
-        </div>
-    )
-}
 export default function MyAccount() {
     return (
         <div className="border border-red-500 w-full px-20 h-[100vh] overflow-y-scroll">
