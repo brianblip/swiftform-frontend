0aa73eb15ffd2e2e2ec7c41aa74cc1779dda8819
Neo <lordneobarnachea@gmail.com>
2024-01-26 18:03:05 +0800

    Change height of container

--- src/components/profile/MyAccount.tsx
+++ src/components/profile/MyAccount.tsx
@@ -42,8 +42,8 @@ export const Input = ({ label, placeholder, type, size, id }: InputProps) => {
 }
 export default function MyAccount() {
     return (
-        <div className="border border-red-500 w-full px-20 py-20 h-max">
-            <p className="text-center text-3xl">My account</p>
+        <div className="border border-red-500 w-full px-20 h-[100vh] overflow-y-scroll">
+            <p className="text-center text-3xl py-10">My account</p>
 
             <ProfileInformation />
             <AccountSettings />
