28e9d12d6bea6656159bf24c8a147f465df2a792
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-28 20:58:15 +0800

    Refactor Single Request API endpoint; Add DELETE operation for remote deletion of generated form.

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -6,23 +6,22 @@ interface Params {
    id: string;
 }
 
-export async function GET(request: Request, { params }: { params: Params }) {
+function findFormDataIndex(id: string): number {
+   return DummyFormData.findIndex((item) => item.id.toString() === id);
+}
+
+export async function GET(
+   request: Request,
+   { params }: { params: Params }
+) {
    const { id } = params;
 
    try {
-      // Check if DummyFormData is an array
-      if (!Array.isArray(DummyFormData)) {
-         throw new Error("Invalid DummyFormData");
-      }
-
-      // Find the form data with the given id
       const formData = DummyFormData.find((item) => item.id.toString() === id);
 
-      if (formData) {
-         return NextResponse.json({ formData }, { status: 200 });
-      } else {
-         return NextResponse.json({ error: "Form not found" }, { status: 404 });
-      }
+      return formData
+         ? NextResponse.json({ formData }, { status: 200 })
+         : NextResponse.json({ error: "Form not found" }, { status: 404 });
    } catch (error) {
       console.error("Error:", error);
       return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
@@ -37,13 +36,31 @@ export async function PATCH(
       const { id } = params;
       const body = await request.json();
 
-      // Find the existing form data
-      const existingFormDataIndex = DummyFormData.findIndex((item) => item.id.toString() === id);
+      const existingFormDataIndex = findFormDataIndex(id);
 
       if (existingFormDataIndex !== -1) {
-         // Update the form data
          DummyFormData[existingFormDataIndex] = { id, ...body };
+         return NextResponse.json({ success: true }, { status: 200 });
+      } else {
+         return NextResponse.json({ error: "Form not found" }, { status: 404 });
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+   }
+}
 
+export async function DELETE(
+   request: Request,
+   { params }: { params: Params }
+) {
+   try {
+      const { id } = params;
+
+      const existingFormDataIndex = findFormDataIndex(id);
+
+      if (existingFormDataIndex !== -1) {
+         DummyFormData.splice(existingFormDataIndex, 1);
          return NextResponse.json({ success: true }, { status: 200 });
       } else {
          return NextResponse.json({ error: "Form not found" }, { status: 404 });
