a863c809bc9d49366b0cd224f53fdfa47daf99b3
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-28 21:07:53 +0800

    Add Features to Dynamic Form creation (Item Order, Item Naming, Item Duplication); Modify code for readability.

--- src/components/DynamicForm.tsx
+++ src/components/DynamicForm.tsx
@@ -1,14 +1,28 @@
 "use client"
-import { useForm, SubmitHandler, useFieldArray, Control } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
+
 import axios from 'axios';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import {
+   useForm,
+   Control,
+   SubmitHandler,
+   useFieldArray,
+} from 'react-hook-form';
+import {
+   Delete,
+   CopyAllOutlined,
+   ArrowUpwardRounded,
+   ArrowDownwardRounded
+} from '@mui/icons-material';
 
 class FormFieldModel {
-   question_type: string = '';
-   question: string = '';
-   required_field: boolean = false;
+   question_name = '';
+   question_type = '';
+   question = '';
+   required_field = false;
 }
+
 interface FormDataModel {
    id: string;
    title: string;
@@ -21,8 +35,8 @@ interface DynamicFormProps extends FormDataModel {
 }
 
 export default function DynamicForm({ id, title, description, fields, onSubmit }: DynamicFormProps) {
-   const { register, control, handleSubmit } = useForm<FormDataModel>({
-      defaultValues: { id, title, description, fields: fields },
+   const { register, control, handleSubmit, setValue } = useForm<FormDataModel>({
+      defaultValues: { id, title, description, fields },
    });
    const router = useRouter();
    const { fields: formFields, append, remove } = useFieldArray<FormFieldModel>({
@@ -30,9 +44,13 @@ export default function DynamicForm({ id, title, description, fields, onSubmit }
       name: 'fields',
    });
 
+   const [tempValues, setTempValues] = useState<FormFieldModel[]>([]);
+
    const handleFormSubmit = async (data: FormDataModel) => {
       try {
          const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+         // Filter out empty question_name values before submitting
+         data.fields = data.fields.filter(field => field.question_name.trim() !== '');
          const response = await axios.patch(`${apiUrl}/${id}`, data);
          console.log('Form submitted successfully:', response.data);
          window.dispatchEvent(new CustomEvent('formCreated'));
@@ -42,89 +60,169 @@ export default function DynamicForm({ id, title, description, fields, onSubmit }
       }
    };
 
-   const appendNewFormField = () => {
-      append(new FormFieldModel());
+   const moveField = (fromIndex: number, toIndex: number) => {
+      formFields.splice(toIndex, 0, formFields.splice(fromIndex, 1)[0]);
+      const movedField = document.getElementById(`fieldset-${toIndex}`);
+      if (movedField) {
+         movedField.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
    };
 
+   const appendNewFormField = () => {
+      const newField = new FormFieldModel();
+      append(newField);
+      setTempValues([...tempValues, { ...newField }]);
+   };
+
+   useEffect(() => {
+      setValue('title', title);
+   }, [title, setValue]);
+
    return (
-      <form onSubmit={handleSubmit(handleFormSubmit)} className="w-1/2 h-full flex flex-col gap-3 border-slate-500">
-         <label htmlFor="title">
-            Title:
-            <input
-               defaultValue={title}
-               {...register('title')}
-               className="border border-slate-500 px-10 py-2 bg-slate-900 text-white"
-               id="title"
-            />
-         </label>
-         <label htmlFor="description">
-            Description:
-            <input
-               defaultValue={description}
-               {...register('description')}
-               className="border border-slate-500 px-10 py-2 bg-slate-900 text-white"
-               id="description"
-            />
-         </label>
+      <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col items-center w-3/4 gap-3 p-10 border border-zinc-500">
+         {/* Description */}
+         <div className="flex flex-col items-center w-4/5 gap-3 p-5">
+            <label htmlFor="description" className='flex flex-col items-center justify-center w-4/5 gap-3'>
+               <textarea
+                  defaultValue={description}
+                  {...register('description')}
+                  className="w-full p-2 text-white rounded min-h-40 max-h-80 bg-primary-secondary"
+                  id="description"
+               ></textarea>
+               Description:
+            </label>
+         </div>
+
+         {/* Form Fields */}
          {formFields.map((field, index) => (
-            <fieldset key={field.id} className='legend border border-slate-500 p-5 flex flex-col gap-3'>
-               <legend className='mx-2 px-2'>Fieldset {index + 1}</legend>
-               <label htmlFor={`fields.${index}.question_type`}>
-                  Select Question Type:
-                  <select
-                     defaultValue={field.question_type}
-                     {...register(`fields.${index}.question_type` as const)}
-                     className='w-1/3 h-10 p-2 bg-slate-900 border border-slate-800 rounded'
-                     id={`fields.${index}.question_type`}
-                  >
-                     <option value="" disabled>Select question type</option>
-                     <option value="textfield">Text Field</option>
-                     <option value="textarea">Text Area</option>
-                     <option value="multiple_choice">Multiple Choice</option>
-                     <option value="checkbox">Checkbox</option>
-                     <option value="dropdown">Dropdown</option>
-                     <option value="attachment">Attachment</option>
-                     <option value="slider">Slider</option>
-                  </select>
-               </label>
-               <label htmlFor={`fields.${index}.question`}>
-                  Insert a question
-                  <input
+            <fieldset
+               key={field.id}
+               id={`fieldset-${index}`}
+               className='flex flex-col w-4/5 gap-5 border rounded-md shadow-xl border-zinc-500 p-7'
+            >
+               <legend className='px-3 mx-5 w-64'>
+                  <label htmlFor={`fields.${index}.question_name`} className='w-full flex text-xs items-center justify-between gap-3'>
+                     Item Name:
+                     <input
+                        defaultValue={field.question_name}
+                        {...register(`fields.${index}.question_name` as const)}
+                        className='w-3/5 p-2 text-primary-white rounded bg-primary-secondary'
+                        id={`fields.${index}.question_name`}
+                     />
+                  </label>
+               </legend>
+               <div className="flex justify-between w-full">
+                  {/* Question Type */}
+                  <label htmlFor={`fields.${index}.question_type`} className='flex flex-col items-start w-3/4 gap-3'>
+                     Type:
+                     <select
+                        defaultValue={field.question_type}
+                        {...register(`fields.${index}.question_type` as const)}
+                        className='w-1/2 h-10 p-2 rounded bg-primary-secondary'
+                        id={`fields.${index}.question_type`}
+                     >
+                        <option value="" disabled>Select question type</option>
+                        <optgroup label='Text Input Fields'>
+                           <option value="text">Text</option>
+                           <option value="email">Email</option>
+                           <option value="password">Password</option>
+                           <option value="textarea">Paragraph</option>
+                        </optgroup>
+                        <optgroup label='Numbers'>
+                           <option value="number">Number</option>
+                           <option value="tel">Phone Number</option>
+                        </optgroup>
+                        <optgroup label='Multiple Choice'>
+                           <option value="select">Dropdown</option>
+                           <option value="checkbox">Checkbox</option>
+                           <option value="radio">Radio Button</option>
+                        </optgroup>
+                        <optgroup label='Slider'>
+                           <option value="range">Slider</option>
+                        </optgroup>
+                        <optgroup label='Date & Time'>
+                           <option value="time">Time</option>
+                           <option value="date">Date</option>
+                           <option value="datetime-local">Date and Time</option>
+                        </optgroup>
+                     </select>
+                  </label>
+
+                  <div className="flex items-start justify-between w-1/4 gap-5 h-fit">
+
+                     {/* Duplicate Buttons */}
+                     <button type="button" onClick={() => append({ ...field })} className="ml-2 w-7 h-7 place-content-center">
+                        <CopyAllOutlined className={`w-full h-full text-white hover:text-blue-600 transition-colors duration-300`} />
+                     </button>
+
+                     {/* Delete Buttons */}
+                     {formFields.length > 1 && (
+                        <button type="button" onClick={() => remove(index)} className="w-7 h-7 place-content-center">
+                           <Delete className={`w-full h-full text-white hover:text-red-600 transition-colors duration-300`} />
+                        </button>
+                     )}
+
+                     {/* Move Field Up and Down Buttons */}
+                     <button
+                        type="button"
+                        onClick={() => {
+                           if (index > 0) {
+                              moveField(index, index - 1);
+                              setValue('fields', [...formFields]); // Update formFields in react-hook-form
+                           }
+                        }}
+                        className="ml-2 w-7 h-7 place-content-center"
+                        disabled={index === 0}
+                     >
+                        <ArrowUpwardRounded className={`w-full h-full text-white hover:text-green-600 transition-colors duration-300`} />
+                     </button>
+
+                     <button
+                        type="button"
+                        onClick={() => {
+                           if (index < formFields.length - 1) {
+                              moveField(index, index + 1);
+                              setValue('fields', [...formFields]); // Update formFields in react-hook-form
+                           }
+                        }}
+                        className="ml-2 w-7 h-7 place-content-center"
+                        disabled={index === formFields.length - 1}
+                     >
+                        <ArrowDownwardRounded className={`w-full h-full text-white hover:text-green-600 transition-colors duration-300`} />
+                     </button>
+                  </div>
+
+               </div>
+
+               {/* Question */}
+               <label htmlFor={`fields.${index}.question`} className='flex flex-col items-start justify-center gap-3'>
+                  Question:
+                  <textarea
                      defaultValue={field.question}
                      {...register(`fields.${index}.question` as const)}
-                     className='w-1/3 h-10 p-2 bg-slate-900 border border-slate-800 rounded'
+                     className='w-full p-2 text-black rounded min-h-40 max-h-80 bg-primary-white'
                      id={`fields.${index}.question`}
-                  />
+                  ></textarea>
                </label>
-               <label htmlFor={`fields.${index}.required_field`}>
-                  Checkbox:
+
+               {/* Required Field Checkbox */}
+               <label htmlFor={`fields.${index}.required_field`} className='flex items-center gap-3'>
                   <input
                      type="checkbox"
                      defaultChecked={field.required_field}
                      {...register(`fields.${index}.required_field` as const)}
                      id={`fields.${index}.required_field`}
                   />
+                  Required:
                </label>
-               {formFields.length > 1 && (
-                  <button
-                     type="button"
-                     onClick={() => remove(index)}
-                     className="bg-red-600 text-white py-2 px-4 w-1/6"
-                  >
-                     Remove Fieldset
-                  </button>
-               )}
             </fieldset>
          ))}
-         <button
-            type="button"
-            onClick={appendNewFormField}
-            className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
-         >
+
+         {/* Add Fieldset and Submit Buttons */}
+         <button type="button" onClick={appendNewFormField} className="px-6 py-3 font-bold text-white bg-green-600 w-fit">
             Add Fieldset
          </button>
-         <button type="submit" className="bg-blue-500 font-bold text-white py-3 px-6 w-fit">Submit</button>
+         <button type="submit" className="px-6 py-3 font-bold text-white bg-blue-500 w-fit">Submit</button>
       </form>
    );
 }
-
