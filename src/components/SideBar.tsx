4feed93b604a868e92b3b9cb1609348c3fb28ca0
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-28 20:59:54 +0800

    Widen the sidebar component to fit the length of form title and other buttons

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -26,27 +26,27 @@ export default function SideBar() {
 }
 
   return (
-    <nav className="hidden h-screen w-60 flex-col items-center p-4 bg-primary-black justify-between md:flex">
+    <nav className="flex-col items-center justify-between hidden h-screen px-2 py-4 min-w-60 max-w-60 bg-primary-black md:flex">
       <Link className="" href="/">
         <p>LOGO</p>
       </Link>
       <FormsList formId={currentFormId}/>
-      <div className="flex flex-col gap-2 w-full">
+      <div className="flex flex-col w-full gap-2">
         <div className="relative">
-          <button onClick={onClickToggleNotification} className="w-full p-2 flex gap-2">
+          <button onClick={onClickToggleNotification} className="flex w-full gap-2 p-2">
             <NotificationsOutlinedIcon />
             <p>Notification</p>
           </button>
           <div className={`absolute top-0 left-[calc(100%+theme(spacing.4))] bg-primary-secondary w-full p-2 ${isNotificationOpen ? 'scale-100' : 'scale-0'} `}><h1>No Notifications</h1></div>
         </div>
         <div className="relative">
-          <button onClick={onClickToggleProfile} className="w-full p-2 flex gap-2">
+          <button onClick={onClickToggleProfile} className="flex w-full gap-2 p-2">
             <PermIdentityOutlinedIcon />
             <p>My Account</p>
           </button>
           <div className={`absolute top-0 left-[calc(100%+theme(spacing.4))] bg-primary-secondary w-full p-2 ${isProfileOpen ? 'scale-100' : 'scale-0'}`}><button>Edit Profile</button></div>
         </div>
-        <button className="w-full p-2 flex gap-2">
+        <button className="flex w-full gap-2 p-2">
           <LogoutOutlinedIcon />
           <p>Log out</p>
         </button>
