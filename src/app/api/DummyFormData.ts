724edba735d89b62d5d9e79dee0c5e79c8528a76
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-29 19:34:41 +0800

    Add 'choices' array for choices rendering

--- src/app/api/DummyFormData.ts
+++ src/app/api/DummyFormData.ts
@@ -1,19 +1,24 @@
 export const DummyFormData = [
    {
-      id: 1,
+      id: "1",
       title: "Test Form Creation",
       description: "Nice Description",
       fields: [
          {
-            question_name: "New Question 1",
-            question_type: "text",
+            question_name: "Select Question",
+            question_type: "select",
             question: "Placeholder Question",
+            choices: [
+               "Option 1", 
+               "Option 2"
+            ],
             required_field: true
          },
          {
             question_name: "New Question 2",
             question_type: "textarea",
             question: "Who Are you?",
+            choices: [],
             required_field: false
          }
       ],
