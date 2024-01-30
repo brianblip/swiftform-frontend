d02980505b5d1e30d252800dec1aeddd34f0a3e0
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-01-30 17:31:17 +0800

    Add question_name variable to Form Fieldset

--- src/app/api/DummyFormData.ts
+++ src/app/api/DummyFormData.ts
@@ -5,11 +5,13 @@ export const DummyFormData = [
       description: "Nice Description",
       fields: [
          {
-            question_type: "textfield",
+            question_name: "New Question 1",
+            question_type: "text",
             question: "Placeholder Question",
             required_field: true
          },
          {
+            question_name: "New Question 2",
             question_type: "textarea",
             question: "Who Are you?",
             required_field: false
