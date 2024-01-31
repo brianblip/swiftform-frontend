5984d2eb25191b1e6e2c9bb56f1476963ad8b8f1
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-31 22:25:18 +0800

    Modify DummyFormData to smoth out inconsistencies

--- src/app/api/DummyFormData.ts
+++ src/app/api/DummyFormData.ts
@@ -1,29 +1,34 @@
 export const DummyFormData = [
   {
-    id: "1",
+    id: 1,
     title: "Test Form Creation",
     description: "Nice Description",
     fields: [
       {
+        field_id: 1,
         question_name: "Select Question",
         question_type: "select",
         question: "Placeholder Question",
-        choices: ["Option 1", "Option 2"],
         required_field: true,
+        choices: ["Option 1", "Option 2"],
       },
       {
+        field_id: 2,
         question_name: "New Question 2",
         question_type: "textarea",
         question: "Who Are you?",
         required_field: false,
+        choices: [],
       },
       {
+        field_id: 3,
         question_name: "New Question 3",
         question_type: "range",
         question: "Who Are you?",
-        minimum: 0,
-        maximum: 10,
         required_field: false,
+        choices: [],
+        minimum: 1,
+        maximum: 10,
       },
     ],
   },
