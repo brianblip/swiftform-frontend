28d1cd8311fd32015418dd6a1985b665477da378
MAKIENAUT <mcrayescoto@gmail.com>
2024-01-31 22:31:56 +0800

    Move Renderable Components to FormBuilderComponents directory

---
+++ src/components/FormBuilderComponents/MultipleChoiceRender.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Delete, AddCircleOutlineOutlined } from '@mui/icons-material';
+
+interface MultipleChoiceRenderProps {
+   choices: string[];
+   onAddChoice: () => void;
+   onRemoveChoice: (index: number) => void;  // Accept choiceIndex as a parameter
+   onUpdateChoice: (index: number, value: string) => void;
+}
+
+export default function MultipleChoiceRender({
+   choices,
+   onAddChoice,
+   onRemoveChoice,
+   onUpdateChoice,
+}: MultipleChoiceRenderProps) {
+   return (
+      <div className="flex flex-col gap-3 py-5">
+         {choices.map((choice, choiceIndex) => (
+            <div key={choiceIndex} className='flex items-center justify-between gap-3'>
+               <label
+                  htmlFor={`choices.${choiceIndex}`}
+                  className='flex items-center justify-start w-full gap-3'
+               >
+                  {String.fromCharCode(65 + choiceIndex)}:
+                  <input
+                     required
+                     defaultValue={choice}
+                     onChange={(e) => onUpdateChoice(choiceIndex, e.target.value)}
+                     className='w-full h-10 p-2 rounded bg-primary-white text-zinc-800'
+                     id={`choices.${choiceIndex}`}
+                  />
+               </label>
+               {choices.length > 1 && ( // Only show delete button when there is more than one choice
+                  <button
+                     type="button"
+                     onClick={() => onRemoveChoice(choiceIndex)}
+                     className="transition-colors ease-in-out text-primary-white hover:text-red-600"
+                  >
+                     <Delete titleAccess='Delete Choice' />
+                  </button>
+               )}
+            </div>
+         ))}
+         <button type="button" onClick={onAddChoice} className="flex items-center gap-1 text-green-500 transition-colors ease-in-out group hover:text-green-600 hover:underline w-fit">
+            <AddCircleOutlineOutlined className='transition-all ease-in-out origin-center group-hover:rotate-90' /> Add Choice
+         </button>
+      </div>
+   );
+}
---
+++ src/components/FormBuilderComponents/SliderRangeRender.tsx
@@ -0,0 +1,50 @@
+import React, { ChangeEvent } from 'react';
+
+interface SliderRangeRenderProps {
+   index: number;
+   minimumValue: number;
+   maximumValue: number;
+   handleMinimumChange: (e: ChangeEvent) => void;
+   handleMaximumChange: (e: ChangeEvent) => void;
+}
+
+export default function SliderRangeRender({
+   index,
+   minimumValue,
+   maximumValue,
+   handleMinimumChange,
+   handleMaximumChange,
+}: SliderRangeRenderProps) {
+   return (
+      <div className="flex flex-col gap-3">
+         <label htmlFor={`fields.${index}.maximum`} className="flex flex-col items-start w-fit gap-3">
+            Maximum:
+            <input
+               min={Number(minimumValue) + 2}
+               max={100}
+               required
+               type="number"
+               pattern="[0-9]*"
+               defaultValue={maximumValue}
+               onChange={handleMaximumChange}
+               placeholder='Set a range of 1-100'
+               className="w-full p-2 text-black rounded bg-primary-white"
+            />
+         </label>
+         <label htmlFor={`fields.${index}.minimum`} className="flex flex-col items-start w-fit gap-3">
+            Minimum:
+            <input
+               min={1}
+               max={Number(maximumValue) - 2}
+               required
+               type="number"
+               pattern="[0-9]*"
+               defaultValue={minimumValue}
+               onChange={handleMinimumChange}
+               placeholder='Set a range of 1-100'
+               className="w-full p-2 text-black rounded bg-primary-white"
+            />
+         </label>
+      </div>
+   );
+}
