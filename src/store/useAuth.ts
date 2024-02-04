b34b4dc312f8c489a15e41115770318f8c2ceb20
Renzo Visperas <renzoviperas07@gmail.com>
2024-02-04 10:53:22 +0800

    update useAuth
    - add loading and error state 
    - add initalize to change user, loading, and error state

--- src/store/useAuth.ts
+++ src/store/useAuth.ts
@@ -1,7 +1,26 @@
 import { create } from "zustand";
+import { User } from "@/types/user";
 
-const useAuth = create((set) => ({
+interface InitializeProps {
+  user?: User | null;
+  isLoading: boolean;
+  error: string | null;
+}
+
+interface AuthState {
+  user?: User | null;
+  isLoading: boolean;
+  error: string | null;
+  initialize: (props: InitializeProps) => void;
+}
+
+const useAuth = create<AuthState>((set) => ({
   user: null,
+  isLoading: false,
+  error: null,
+  initialize: ({ user, isLoading, error }: InitializeProps) => {
+    set({ user, isLoading, error });
+  },
 }));
 
 export default useAuth;
