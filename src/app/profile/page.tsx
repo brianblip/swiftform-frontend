6ad7f9b8a4828b8a61283df68d3389efbfabeba3
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-06 15:29:25 +0800

    Reformat files with prettier

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,15 @@
-'use client'
+"use client";
 
-import ProfileInformation from "./ProfileInformation"
-import AccountSettings from "./AccountSettings"
-
+import ProfileInformation from "./ProfileInformation";
+import AccountSettings from "./AccountSettings";
 
 export default function MyAccount() {
     return (
-        <div className="w-full px-20 h-[100vh] overflow-y-scroll">
-            <p className="text-center text-3xl py-10">My account</p>
+        <div className="h-[100vh] w-full overflow-y-scroll px-20">
+            <p className="py-10 text-center text-3xl">My account</p>
 
             <ProfileInformation />
             <AccountSettings />
         </div>
-    )
+    );
 }
