1eb352c317c3bff31ae95d971f47bbc9e7e402cc
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-06 17:00:18 +0800

    update ProfileInformation design

--- src/app/profile/ProfileInformation.tsx
+++ src/app/profile/ProfileInformation.tsx
@@ -2,7 +2,7 @@ import Input from "../../components/Input";
 
 export default function ProfileInformation() {
     return (
-        <form className="flex w-full flex-col gap-4 md:w-3/4">
+        <form className="flex w-full flex-col gap-4">
             <p className="text-xl font-medium">Profile Information</p>
             <div className="flex items-center justify-center gap-10">
                 <div className="flex aspect-square h-[120px] items-center justify-center rounded-full border border-red-500"></div>
@@ -10,7 +10,7 @@ export default function ProfileInformation() {
                     Change picture
                 </button>
             </div>
-            <div className="flex justify-center gap-10">
+            <div className="flex gap-10">
                 <Input
                     type="name"
                     placeholder="First name"
@@ -26,15 +26,13 @@ export default function ProfileInformation() {
                     id="lastName"
                 />
             </div>
-            <div className="flex justify-center gap-10">
-                <Input
-                    type="email"
-                    placeholder="Email"
-                    label="Email"
-                    size="full"
-                    id="email"
-                />
-            </div>
+            <Input
+                type="email"
+                placeholder="Email"
+                label="Email"
+                size="full"
+                id="email"
+            />
             <button className="w-fit rounded bg-primary-secondary px-8 py-2">
                 Save Changes
             </button>
