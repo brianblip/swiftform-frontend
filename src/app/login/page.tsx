bc30cc854eb14cae6c85929c723c4e005f7da7d1
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-06 14:58:06 +0800

    Replace comments with TODO to specify the further action items

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
-import Image from "next/image"
-import Birdie from "../../assets/Birdie.png"
-import { useForm, Controller } from 'react-hook-form';
-
+import Image from "next/image";
+import Birdie from "../../assets/Birdie.png";
+import { useForm, Controller } from "react-hook-form";
 
 type FormData = {
     email: string;
@@ -12,14 +11,12 @@ type FormData = {
 };
 
 export default function Login() {
-
     const router = useRouter();
 
     const { handleSubmit, control, setError, reset } = useForm<FormData>();
 
     const onSubmit = (data: FormData) => {
-        //logic here
-        console.log(data);
+        // TODO: Handle form submission
         reset({
             email: "",
             password: "",
@@ -27,15 +24,23 @@ export default function Login() {
     };
 
     return (
-        <div className="h-[100vh] bg-white w-screen flex flex-col justify-center items-center min-w-[352px]">
-            <div className="border rounded-lg border-gray-300 w-[80%] md:w-[50%] h-[85%] flex flex-col items-center gap-9 justify-center max-h-[1300px]">
-                <div className="w-28 h-36 rounded-full overflow-hidden" >
+        <div className="flex h-[100vh] w-screen min-w-[352px] flex-col items-center justify-center bg-white">
+            <div className="flex h-[85%] max-h-[1300px] w-[80%] flex-col items-center justify-center gap-9 rounded-lg border border-gray-300 md:w-[50%]">
+                <div className="h-36 w-28 overflow-hidden rounded-full">
                     <Image src={Birdie} alt="Swift form logo" />
                 </div>
-                <p className="text-center font-extrabold text-4xl text-black">Welcome Back</p>
-                <form onSubmit={handleSubmit(onSubmit)} className="w-full flex flex-col justify-center items-center">
+                <p className="text-center text-4xl font-extrabold text-black">
+                    Welcome Back
+                </p>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex w-full flex-col items-center justify-center"
+                >
                     <div className="mb-4 w-[60%] max-w-md">
-                        <label className="block text-sm font-bold text-black" htmlFor="email">
+                        <label
+                            className="block text-sm font-bold text-black"
+                            htmlFor="email"
+                        >
                             Email
                         </label>
                         <Controller
@@ -45,26 +50,33 @@ export default function Login() {
                                 <>
                                     <input
                                         {...field}
-                                        className="border h-[52px] border-black w-full py-2 px-3 text-gray-700 leading-tight rounded-[4px]  focus:shadow-outline"
+                                        className="focus:shadow-outline h-[52px] w-full rounded-[4px] border border-black px-3 py-2 leading-tight  text-gray-700"
                                         id="email"
                                         placeholder="Email address"
                                         type="email"
                                         required
                                     />
-                                    {fieldState.error && <p className="text-red-500">{fieldState.error.message}</p>}
+                                    {fieldState.error && (
+                                        <p className="text-red-500">
+                                            {fieldState.error.message}
+                                        </p>
+                                    )}
                                 </>
                             )}
                             rules={{
-                                required: 'Email is required',
+                                required: "Email is required",
                                 pattern: {
                                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                                    message: 'Invalid email address',
+                                    message: "Invalid email address",
                                 },
                             }}
                         />
                     </div>
                     <div className="mb-4 w-[60%] max-w-md">
-                        <label className='block text-sm font-bold text-black' htmlFor='password'>
+                        <label
+                            className="block text-sm font-bold text-black"
+                            htmlFor="password"
+                        >
                             Password
                         </label>
                         <Controller
@@ -74,36 +86,46 @@ export default function Login() {
                                 <>
                                     <input
                                         {...field}
-                                        className="border h-[52px] border-black w-full py-2 px-3 text-gray-700 leading-tight rounded-[4px]"
+                                        className="h-[52px] w-full rounded-[4px] border border-black px-3 py-2 leading-tight text-gray-700"
                                         type="password"
                                         placeholder="********"
                                         id="password"
                                         required
                                     />
-                                    {fieldState.error && <p className="text-red-500">{fieldState.error.message}</p>}
+                                    {fieldState.error && (
+                                        <p className="text-red-500">
+                                            {fieldState.error.message}
+                                        </p>
+                                    )}
                                 </>
                             )}
                             rules={{
-                                required: 'Password is required',
+                                required: "Password is required",
                                 minLength: {
                                     value: 6,
-                                    message: 'Password must be at least 6 characters',
+                                    message:
+                                        "Password must be at least 6 characters",
                                 },
                             }}
                         />
                     </div>
-                    <p className="text-black cursor-pointer w-[60%] mb-4">Forgot password?</p>
+                    <p className="mb-4 w-[60%] cursor-pointer text-black">
+                        Forgot password?
+                    </p>
                     <button
                         type="submit"
-                        className="border w-[60%] max-w-md rounded-md h-[52px] border-black py-2 px-3 bg-black text-white leading-tight"
+                        className="h-[52px] w-[60%] max-w-md rounded-md border border-black bg-black px-3 py-2 leading-tight text-white"
                     >
                         Continue
                     </button>
                 </form>
-                <p className="text-center text-black cursor-pointer" onClick={() => router.push('/signup')}>
+                <p
+                    className="cursor-pointer text-center text-black"
+                    onClick={() => router.push("/signup")}
+                >
                     Don't have an account? Signup
                 </p>
             </div>
         </div>
-    )
+    );
 }
