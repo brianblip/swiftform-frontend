a1b98f124a9fb95df005f7883435932adc486ec6
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-06 14:56:43 +0800

    Redesign profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,11 +5,12 @@ import AccountSettings from "./AccountSettings";
 
 export default function MyAccount() {
     return (
-        <main className="flex w-full flex-col items-center gap-4 overflow-y-scroll p-8">
+        <main className="flex w-full flex-col items-center gap-4 p-8">
             <h1 className="text-3xl font-bold">My account</h1>
-
-            <ProfileInformation />
-            <AccountSettings />
+            <section className="flex w-full max-w-fit flex-col items-center gap-8">
+                <ProfileInformation />
+                <AccountSettings />
+            </section>
         </main>
     );
 }
