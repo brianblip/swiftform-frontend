a541cc27690def190a27900151c95f23fb1ba42f
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-06 16:54:15 +0800

    Update ProfileInformation design

--- src/app/profile/ProfileInformation.tsx
+++ src/app/profile/ProfileInformation.tsx
@@ -2,15 +2,15 @@ import Input from "../../components/Input";
 
 export default function ProfileInformation() {
     return (
-        <form>
-            <p className="text-xl">Profile Information</p>
-            <div className="flex h-[20vh] items-center gap-10 lg:pl-48">
-                <div className="h-[120px] w-[120px] rounded-full border border-red-500"></div>
-                <button className="flex h-[44px] w-[176px] items-center justify-center rounded-sm bg-[#444654]">
+        <form className="flex w-full flex-col gap-4 md:w-3/4">
+            <p className="text-xl font-medium">Profile Information</p>
+            <div className="flex items-center justify-center gap-10">
+                <div className="flex aspect-square h-[120px] items-center justify-center rounded-full border border-red-500"></div>
+                <button className="cursor-pointer rounded bg-primary-secondary px-4 py-2 text-center">
                     Change picture
                 </button>
             </div>
-            <div className="flex flex-row justify-center gap-10">
+            <div className="flex justify-center gap-10">
                 <Input
                     type="name"
                     placeholder="First name"
@@ -26,7 +26,7 @@ export default function ProfileInformation() {
                     id="lastName"
                 />
             </div>
-            <div className="flex flex-row justify-center gap-10">
+            <div className="flex justify-center gap-10">
                 <Input
                     type="email"
                     placeholder="Email"
@@ -35,11 +35,9 @@ export default function ProfileInformation() {
                     id="email"
                 />
             </div>
-            <div className="flex justify-end">
-                <button className="flex h-[44px] w-[176px] items-center justify-center rounded-sm bg-[#444654]">
-                    Save Changes
-                </button>
-            </div>
+            <button className="w-fit rounded bg-primary-secondary px-8 py-2">
+                Save Changes
+            </button>
         </form>
     );
 }
