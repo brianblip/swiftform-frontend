4913d7137474d4ab9670e563c6a9243799b23929
MAKIENAUT <mcrayescoto@gmail.com>
2024-02-08 17:46:51 +0800

    Refactor AuthProvider to use isAuthenticated status for page protection; Utilize useEffect hook for life cycle

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,29 +1,44 @@
 "use client";
 
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect } from "react";
 import useSWR from "swr";
 import fetcher from "@/utils/fetcher";
 import { User } from "@/types/user";
 import useAuth from "@/store/useAuth";
+import { useRouter } from "next/navigation";
 
 export const AuthContext = createContext(null);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-    const { isLoading, error } = useSWR<{
-        data: User;
-    }>(`/users/me`, fetcher, {
-        onSuccess: (response) => {
-            const { data } = response;
-            initialize({
-                user: data,
-                isLoading,
-                error,
-            });
-        },
-    });
+    const router = useRouter();
+    const { data: userData, error: userError } = useSWR<User>(
+        `/users/me`,
+        fetcher,
+    );
 
     const { initialize } = useAuth();
 
+    useEffect(() => {
+        if (userData) {
+            initialize({
+                user: userData,
+                isLoading: false,
+                error: null,
+                isAuthenticated: true,
+            });
+        }
+    }, [userData, initialize]);
+
+    if (!userData && !userError) {
+        return <div>Loading...</div>;
+    }
+
+    if (userError) {
+        console.error("Error fetching user data", userError);
+        router.push("/Login");
+    }
+
     return <AuthContext.Provider value={null}>{children}</AuthContext.Provider>;
 }
+
 export default AuthContext;
