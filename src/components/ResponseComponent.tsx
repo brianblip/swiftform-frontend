a1fa41e4ad5e87ceece16f0d700afeccfb8155c7
MAKIENAUT <mcrayescoto@gmail.com>
2024-02-13 22:34:05 +0800

    [Experimental Component] - No use case yet

--- src/components/ResponseComponent.tsx
+++ src/components/ResponseComponent.tsx
@@ -1,10 +1,23 @@
 // ResponseComponent.tsx
 import React from "react";
 
-export default function ResponseComponent() {
+export interface ResponseProps {
+    user_id: number;
+    owner_id: number;
+}
+
+export default function ResponseComponent({
+    user_id,
+    owner_id,
+}: ResponseProps) {
+    if (user_id !== owner_id) {
+        return <p>You are not authorized to access this form.</p>;
+    }
     return (
         <div>
             <h2>Response Section</h2>
+            {/* You can now access user_id and titleInput in this component */}
+            <p>User ID: {user_id}</p>
         </div>
     );
 }
