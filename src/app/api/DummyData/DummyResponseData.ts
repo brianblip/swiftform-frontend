f990813f61f283866391fc11ad51f7bc58a68038
MAKIENAUT <mcrayescoto@gmail.com>
2024-02-13 22:38:32 +0800

    Create new dummy data for responses; Move to DummyData folder to be organized;

---
+++ src/app/api/DummyData/DummyResponseData.ts
@@ -0,0 +1,52 @@
+export const DummyResponseData = [
+    {
+        id: 2,
+        owner_id: 6,
+        responses: [
+            {
+                respondent: {
+                    id: 1,
+                    name: "John Doe",
+                    email: "john@example.com",
+                    avatar_url: "",
+                },
+                answers: [
+                    {
+                        field_id: 1,
+                        answer: "Option 1",
+                    },
+                    {
+                        field_id: 2,
+                        answer: "I am John Doe.",
+                    },
+                    {
+                        field_id: 3,
+                        answer: 8,
+                    },
+                ],
+            },
+            {
+                respondent: {
+                    id: 2,
+                    name: "Jane Smith",
+                    email: "jane@example.com",
+                    avatar_url: "",
+                },
+                answers: [
+                    {
+                        field_id: 1,
+                        answer: "Option 2",
+                    },
+                    {
+                        field_id: 2,
+                        answer: "I am Jane Smith.",
+                    },
+                    {
+                        field_id: 3,
+                        answer: 6,
+                    },
+                ],
+            },
+        ],
+    },
+];
