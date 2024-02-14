3337bf43b7f21f6c803904c013cf8db7dafb7dce
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-14 14:43:54 +0800

    Refactor design and structure of login page

--- src/app/(auth)/Login/page.tsx
+++ src/app/(auth)/Login/page.tsx
@@ -6,93 +6,84 @@ import Image from "next/image";
 import Birdie from "@/assets/Birdie.png";
 import useAuth from "@/store/useAuth";
 import { NextRequest, NextResponse } from "next/server";
+import Link from "next/link";
 
 export default function Login(req: NextRequest, res: NextResponse) {
     const router = useRouter();
-    const { login, logout } = useAuth();
-    const [email, setEmail] = useState(""); // State for email input
+    const { login } = useAuth();
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const handleLogin = async (e: { preventDefault: () => void }) => {
-        e.preventDefault(); // Prevent default form submission behavior
-
-        // Call login function from useAuth hook with email and password
         const success = await login({
             email: email,
             password: password,
         });
 
-        // If login is successful, redirect to home page
         if (success) {
             router.push("/");
         }
     };
 
     return (
-        <div className="flex h-[100vh] w-screen min-w-[352px] flex-col items-center justify-center bg-white">
-            <div className="flex h-[85%] max-h-[1300px] w-[80%] flex-col items-center justify-center gap-9 rounded-lg border border-gray-300 md:w-[50%]">
-                <div className="h-36 w-28 overflow-hidden rounded-full">
-                    <Image src={Birdie} alt="Swift form logo" />
+        <div className="flex min-h-screen w-full items-center justify-center bg-white text-black">
+            <div className="flex flex-col items-center">
+                <div className="mb-8 h-28 w-28 overflow-hidden rounded-full">
+                    <Image src={Birdie} alt="SwiftForm logo" />
                 </div>
-                <p className="text-center text-4xl font-extrabold text-black">
-                    Welcome Back
-                </p>
-                <form
-                    onSubmit={handleLogin} // Call handleLogin on form submission
-                    className="flex w-full flex-col items-center justify-center"
-                >
-                    <div className="mb-4 w-[60%] max-w-md">
-                        <label
-                            className="block text-sm font-bold text-black"
-                            htmlFor="email"
-                        >
+
+                <h1 className="mb-8 text-xl font-bold md:text-2xl">
+                    Welcome back
+                </h1>
+
+                <form onSubmit={handleLogin} className="grid gap-4">
+                    <div className="grid gap-1">
+                        <label htmlFor="email" className="text-sm font-medium">
                             Email
                         </label>
                         <input
-                            className="focus:shadow-outline h-[52px] w-full rounded-[4px] border border-black px-3 py-2 leading-tight text-gray-700"
-                            id="email"
-                            placeholder="Email address"
                             type="email"
                             required
-                            value={email} // Bind value to email state
-                            onChange={(e) => setEmail(e.target.value)} // Update email state on change
+                            className="focus:shadow-outline rounded border border-black px-3 py-3"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
-                    <div className="mb-4 w-[60%] max-w-md">
+
+                    <div className="grid gap-1">
                         <label
-                            className="block text-sm font-bold text-black"
                             htmlFor="password"
+                            className="text-sm font-medium"
                         >
                             Password
                         </label>
                         <input
-                            className="h-[52px] w-full rounded-[4px] border border-black px-3 py-2 leading-tight text-gray-700"
                             type="password"
-                            placeholder="********"
-                            id="password"
                             required
-                            value={password} // Bind value to password state
-                            onChange={(e) => setPassword(e.target.value)} // Update password state on change
+                            className="focus:shadow-outline rounded border border-black px-3 py-3"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
-                        <button className="mb-4 w-[60%] cursor-pointer text-black">
-                            Forgot password?
-                        </button>
                     </div>
+
+                    <a href="#" className="mb-4 text-sm font-medium">
+                        Forgot password?
+                    </a>
+
                     <button
                         type="submit"
-                        className="h-[52px] w-[60%] max-w-md rounded-md border border-black bg-black px-3 py-2 leading-tight text-white"
+                        className="mb-4 w-full rounded bg-black py-3 text-white"
                     >
                         Continue
                     </button>
+
+                    <Link href="" className="text-center text-sm">
+                        Don&rsquo;t have an account?
+                        <span className="ml-1 font-medium">Sign up</span>
+                    </Link>
                 </form>
-                <p
-                    className="cursor-pointer text-center text-black"
-                    onClick={() => {
-                        router.push("/Register");
-                    }}
-                >
-                    Don't have an account? Signup
-                </p>
             </div>
         </div>
     );
