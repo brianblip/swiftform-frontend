1672348764e7117dbe776d2d75d33d6a5d10b6e9
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-16 10:00:16 +0800

    Remove unnecessary bloat from the DOM tree

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -5,12 +5,10 @@ import AccountSettings from "./AccountSettings";
 
 export default function MyAccount() {
     return (
-        <main className="flex w-full flex-col items-center gap-4 p-8">
+        <main className="flex w-dvw flex-col items-center gap-8 p-8">
             <h1 className="text-3xl font-bold">My account</h1>
-            <section className="flex w-full max-w-fit flex-col items-center gap-8">
-                <ProfileInformation />
-                <AccountSettings />
-            </section>
+            <ProfileInformation />
+            <AccountSettings />
         </main>
     );
 }
