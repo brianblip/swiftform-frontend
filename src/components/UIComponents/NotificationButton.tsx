f8971061cc9eb9f19f2e074b3d30792e439465be
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-22 16:54:03 +0800

    add useEffect and useRef to create functionality of closing the panel when clicked outside its elements

--- src/components/UIComponents/NotificationButton.tsx
+++ src/components/UIComponents/NotificationButton.tsx
@@ -1,16 +1,38 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 
 const NotificationButton = () => {
     const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+    const parentRef = useRef<HTMLDivElement>(null);
 
     function onClickToggleNotification() {
         setIsNotificationOpen(!isNotificationOpen);
     }
 
+    useEffect(() => {
+        function onClickCloseNotification(e: MouseEvent) {
+            if (
+                parentRef.current &&
+                !parentRef.current.contains(e.target as Node)
+            ) {
+                setIsNotificationOpen(false);
+            }
+        }
+
+        if (isNotificationOpen) {
+            window.addEventListener("click", onClickCloseNotification);
+        } else {
+            window.removeEventListener("click", onClickCloseNotification);
+        }
+
+        return function () {
+            window.removeEventListener("click", onClickCloseNotification);
+        };
+    }, [isNotificationOpen]);
+
     return (
-        <div>
+        <div ref={parentRef}>
             <button
                 onClick={onClickToggleNotification}
                 className={`p-4 hover:bg-primary-secondary ${isNotificationOpen ? "bg-primary-secondary" : ""}`}
