53b45c299940a495de518ec646d84196d30c2c49
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-22 16:44:03 +0800

    add the missing elements from topbar to component (no functionality yet)

--- src/components/UIComponents/NotificationButton.tsx
+++ src/components/UIComponents/NotificationButton.tsx
@@ -1,5 +1,20 @@
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+
 const NotificationButton = () => {
-    return <h1 className="p-2">No Notifications</h1>;
+    return (
+        <div>
+            <button
+                className={`p-4 hover:bg-primary-secondary`}
+            >
+                <NotificationsOutlinedIcon />
+            </button>
+            <div
+                className={`absolute left-0 top-full w-full bg-primary-secondary p-2`}
+            >
+                <h1 className="p-2">No Notifications</h1>
+            </div>
+        </div>
+    );
 };
 
 export default NotificationButton;
