7e6790e6eb72a5178df76677d174caea74f04d44
Renzo Visperas <renzoviperas07@gmail.com>
2024-02-22 13:01:59 +0800

    add custom required and error prop to input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,23 +1,30 @@
 import { forwardRef } from "react";
 import { TextField, TextFieldProps } from "@mui/material";
 
-export const Input = forwardRef<HTMLInputElement, TextFieldProps>(
-    (props, ref) => {
-        return (
-            <TextField
-                {...props}
-                inputRef={ref}
-                InputProps={{
-                    style: {
-                        backgroundColor: "#fff",
-                        ...props.InputProps?.style,
-                    },
-                }}
-                size="small"
-                fullWidth
-            />
-        );
-    },
-);
+interface Props extends Omit<TextFieldProps, "error" | "required"> {
+    error?: string;
+    required?: boolean;
+}
+
+export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
+    return (
+        <TextField
+            size="small"
+            fullWidth
+            {...props}
+            inputRef={ref}
+            InputProps={{
+                style: {
+                    backgroundColor: "#fff",
+                    ...props.InputProps?.style,
+                },
+            }}
+            error={!!props.error}
+            helperText={props.error}
+            required={undefined} // don't use required prop because it's overlapping with react-hook-form
+            label={`${props.label} ${props.required ? "*" : ""}`}
+        />
+    );
+});
 
 Input.displayName = "Input";
