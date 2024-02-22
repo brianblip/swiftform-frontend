35e6d53356b52e48812cc7fed75292396d9e272f
Renzo Visperas <renzoviperas07@gmail.com>
2024-02-22 12:47:48 +0800

    add forwardRef to input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,16 +1,22 @@
+import { forwardRef } from "react";
 import { TextField, TextFieldProps } from "@mui/material";
 
-export const Input = (props: TextFieldProps) => {
-    return (
-        <TextField
-            {...props}
-            style={{
-                backgroundColor: "#fff",
-                borderRadius: "4px",
-                ...props.style,
-            }}
-            size="small"
-            fullWidth
-        />
-    );
-};
+export const Input = forwardRef<HTMLInputElement, TextFieldProps>(
+    (props, ref) => {
+        return (
+            <TextField
+                {...props}
+                inputRef={ref}
+                style={{
+                    backgroundColor: "#fff",
+                    borderRadius: "4px",
+                    ...props.style,
+                }}
+                size="small"
+                fullWidth
+            />
+        );
+    },
+);
+
+Input.displayName = "Input";
