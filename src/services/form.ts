050b78992e4f019992eb6e96ddbdd073d7f7a040
Renzo Visperas <renzoviperas07@gmail.com>
2024-02-22 12:53:58 +0800

    add CreateFormOptions interface to 'create form' service

--- src/services/form.ts
+++ src/services/form.ts
@@ -1,7 +1,11 @@
 import api from "@/services/api";
 import { Form, ApiResponse } from "@@/types";
 
-export const createForm = async (name: Form["name"]) => {
+export interface CreateFormOptions {
+    name: Form["name"];
+}
+
+export const createForm = async ({ name }: CreateFormOptions) => {
     const { data } = await api.post<ApiResponse<Form>>("/forms", { name });
     return data;
 };
