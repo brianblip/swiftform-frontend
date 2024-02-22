a7d335821ac1e60cdc4ff8beb6e190dd5d248908
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-22 17:11:41 +0800

    add missing elements to component

--- src/components/UIComponents/ProfileButton.tsx
+++ src/components/UIComponents/ProfileButton.tsx
@@ -1,5 +1,27 @@
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import Link from "next/link";
+
 const ProfileButton = () => {
-    return <h1>Profile Button</h1>;
+    return (
+        <div>
+            <button
+                className={`flex w-full gap-2 p-2 hover:bg-primary-secondary`}
+            >
+                <PermIdentityOutlinedIcon />
+                <p>My Account</p>
+            </button>
+            <div
+                className={`absolute bottom-full mb-2 w-full flex-col items-start bg-primary-secondary p-2`}
+            >
+                <Link
+                    href="/profile"
+                    className="w-full p-2 text-start hover:bg-primary-neutral"
+                >
+                    Edit Profile
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default ProfileButton;
