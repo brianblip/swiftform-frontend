31a48f2ae4dd1c49456ccb19e7c9e9b7deb8440b
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-23 13:29:22 +0800

    add the functionality of closing the alert when it appears (no passive way for alert to appear yet)

--- src/components/CustomAlert.tsx
+++ src/components/CustomAlert.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import CloseIcon from "@mui/icons-material/Close";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
+import { useState } from "react";
 
 export default function CustomAlert() {
+    const [isAlertVisible, setIsAlertVisible] = useState(Boolean);
+
+    function onClickCloseAlert() {
+        setIsAlertVisible(false);
+    }
+
     return (
-            <section className={`absolute bottom-4 right-4 flex max-h-16 min-w-48 max-w-[328px] gap-2 rounded p-4 text-primary-black`}>
-                <button className={`absolute right-0.5 top-0.5 grid place-items-center rounded hover:bg-primary-black/10`}>
+        isAlertVisible && (
+            <section
+                className={`absolute bottom-4 right-4 flex max-h-16 min-w-48 max-w-[328px] gap-2 rounded p-4 text-primary-black`}
+            >
+                <button
+                    onClick={onClickCloseAlert}
+                    className={`absolute right-0.5 top-0.5 grid place-items-center rounded hover:bg-primary-black/10`}
+                >
                     <CloseIcon className="text-sm" />
                 </button>
                     <CheckCircleIcon className="text-green-500" />
@@ -16,5 +31,6 @@ export default function CustomAlert() {
                     </h1>
                 </div>
             </section>
+        )
     );
 }
