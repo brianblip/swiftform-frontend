872a4f5c667107ac2a402cdd9fad28a79339bb1a
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-23 11:55:40 +0800

    Add tailwind styling to the components

--- src/components/CustomAlert.tsx
+++ src/components/CustomAlert.tsx
@@ -4,14 +4,14 @@ import CancelIcon from "@mui/icons-material/Cancel";
 
 export default function CustomAlert() {
     return (
-            <section>
-                <button>
-                    <CloseIcon />
+            <section className={`absolute bottom-4 right-4 flex max-h-16 min-w-48 max-w-[328px] gap-2 rounded p-4 text-primary-black`}>
+                <button className={`absolute right-0.5 top-0.5 grid place-items-center rounded hover:bg-primary-black/10`}>
+                    <CloseIcon className="text-sm" />
                 </button>
-                    <CheckCircleIcon />
-                    <CancelIcon />
-                <div>
-                    <h1>
+                    <CheckCircleIcon className="text-green-500" />
+                    <CancelIcon className=" text-primary-white" />
+                <div className="flex gap-2 overflow-x-scroll whitespace-nowrap">
+                    <h1 className={`font-semibold`}>
                         Custom Alert
                     </h1>
                 </div>
