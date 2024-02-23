3919e90b6c93142c80280d8dd997aac813416c12
Sly Villafania <villafania.sly.lau@gmail.com>
2024-02-23 11:46:36 +0800

    add basic elements to component

--- src/components/CustomAlert.tsx
+++ src/components/CustomAlert.tsx
@@ -1,3 +1,20 @@
+import CloseIcon from "@mui/icons-material/Close";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
+
 export default function CustomAlert() {
-    return <p>Custom Alert</p>;
+    return (
+            <section>
+                <button>
+                    <CloseIcon />
+                </button>
+                    <CheckCircleIcon />
+                    <CancelIcon />
+                <div>
+                    <h1>
+                        Custom Alert
+                    </h1>
+                </div>
+            </section>
+    );
 }
