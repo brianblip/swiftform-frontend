bdce4e5e169f313f5b81200ba147c2627ba8b972
Renzo Visperas <renzoviperas07@gmail.com>
2024-02-27 20:35:36 +0800

    fetch form using swr

--- src/app/(main)/Form/[formId]/page.tsx
+++ src/app/(main)/Form/[formId]/page.tsx
@@ -2,27 +2,22 @@
 
 import { Edit } from "@mui/icons-material";
 import React, { useState, useEffect } from "react";
-import useForm, { FormProvider } from "@/contexts/singleForm";
 import ResponseComponent from "@/components/ResponseComponent";
-import { FormParam } from "@@/types";
+import useSWR from "swr";
+import { Form } from "@@/types";
+import { fetcher } from "@/utils";
+import { ErrorBoundary } from "@/components";
 
-export default function FormPage({ params }: { params: FormParam }) {
+export default function FormPage({ params }: { params: { formId: string } }) {
     const { formId } = params;
-    const { form, isLoading, error, fetchForm } = useForm(formId);
+    const {
+        data: form,
+        isLoading,
+        error,
+    } = useSWR<Form>(`/forms/${formId}`, fetcher);
+
     const [isQuestionSectionOpen, setIsQuestionSectionOpen] = useState(true);
     const [titleInput, setTitleInput] = useState<string>("");
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await fetchForm(formId);
-            } catch (error) {
-                console.error("Error fetching form data:", error);
-            }
-        };
-
-        fetchData();
-    }, [fetchForm, formId]);
 
     useEffect(() => {
         if (form) {
@@ -49,20 +44,8 @@ export default function FormPage({ params }: { params: FormParam }) {
     const mainClassNames =
         "h-[calc(100vh-57.0667px)] w-screen p-4 pt-16 sm:p-8 sm:pt-16 md:h-screen overflow-scroll flex flex-col items-center gap-10";
 
-    if (isLoading) {
-        return <main className={mainClassNames}>Loading...</main>;
-    }
-
-    if (error) {
-        return <main className={mainClassNames}>Error: {error}</main>;
-    }
-
-    if (!form) {
-        return <main className={mainClassNames}>No form found.</main>;
-    }
-
     return (
-        <FormProvider>
+        <ErrorBoundary isLoading={isLoading} error={error}>
             <main className={mainClassNames}>
                 <div className="flex w-full flex-col items-center gap-y-4">
                     <div className="flex w-full items-center justify-center">
@@ -93,14 +76,14 @@ export default function FormPage({ params }: { params: FormParam }) {
                 {isQuestionSectionOpen ? (
                     <>
                         <h1>
-                            {form.formId} {form.name}
+                            {form?.id} {form?.name}
                         </h1>
-                        <p>{form.description}</p>
+                        <p>{form?.description}</p>
                     </>
                 ) : (
                     <ResponseComponent />
                 )}
             </main>
-        </FormProvider>
+        </ErrorBoundary>
     );
 }
