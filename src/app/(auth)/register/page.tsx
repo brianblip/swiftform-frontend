e9b326ca29b527a22ef526ff60fafcf08420ed04
Sly Villafania <villafania.sly.lau@gmail.com>
2024-03-05 13:36:08 +0800

    refactor register inputs to utilize Input Component

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -74,156 +74,80 @@ export default function RegistrationPage() {
                     Create your account
                 </h1>
 
-                <form onSubmit={handleSubmit(handleFormSubmit)} className="grid gap-4">
-                    <div className="grid gap-1">
-                        <label htmlFor="name" className="text-sm font-medium">
-                            {errors.name ? (
-                                <span className="text-red-500">
-                                    {errors.name.message}
-                                </span>
-                            ) : "Name"}                        </label>
-                        <input
-                            type="text"
-                            className={`rounded border ${errors.name ? "border-red-500" : "border-black"
-                                } p-3`}
-                            id="name"
-                            {...formRegister("name", {
-                                required: "Name is required",
-                                minLength: {
-                                    value: 2,
-                                    message: "At least 2 characters long"
-                                },
-                                maxLength: {
-                                    value: 50,
-                                    message: "Name should not exceed 50 characters"
-                                }
-                            })}
-                            aria-invalid={errors.name ? "true" : "false"}
-                        />
-                    </div>
-
-                    <div className="grid gap-1">
-                        <label htmlFor="email" className="text-sm font-medium">
-                            {errorMessage ? (
-                                <span className="text-red-500">
-                                    {errorMessage}
-                                </span>
-                            ) : errors.email ? (
-                                <span className="text-red-500">
-                                    {errors.email.message}
-                                </span>
-                            ) : "Email"}
-                        </label>
-                        <input
-                            type="email"
-                            className={`rounded border ${errors.email || errorMessage ? "border-red-500" : "border-black"
-                                } p-3`}
-                            id="email"
-                            {...formRegister("email", {
-                                required: "Email is required",
-                                pattern: {
-                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                                    message: "Invalid email address"
-                                },
-                            })}
-                            aria-invalid={errors.email ? "true" : "false"}
-                        />
-                    </div>
-
-                    <div className="grid gap-1">
-                        <label
-                            htmlFor="password"
-                            className="text-sm font-medium"
-                        >
-                            {errors.password ? (
-                                <span className="text-red-500">
-                                    {errors.password.message}
-                                </span>
-                            ) : "Password"}
-                        </label>
-                        <div className="relative flex items-center">
-                            <input
-                                type={isPasswordVisible ? "text" : "password"}
-                                required
-                                className={`rounded border ${errors.password ? "border-red-500" : "border-black"
-                                    } p-3 pr-10`}
-                                id="password"
-                                {...formRegister("password", {
-                                    required: "Password is required",
-                                    minLength: {
-                                        value: 8,
-                                        message: "Password must be 8 characters"
-                                    }
-                                })}
-                                aria-invalid={errors.password ? "true" : "false"}
-                            />
-                            {isPasswordVisible ? (
-                                <button
-                                    onClick={onClickTogglePasswordVisibility}
-                                    className="absolute right-0 mr-2 hover:text-primary-secondary"
-                                >
-                                    <VisibilityOffIcon />
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={onClickTogglePasswordVisibility}
-                                    className="absolute right-0 mr-2 hover:text-primary-secondary"
-                                >
-                                    <VisibilityIcon />
-                                </button>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className="mb-4 grid gap-1">
-                        <label
-                            htmlFor="confirm_password"
-                            className="text-sm font-medium"
-                        >
-                            {errors.confirmPassword ? (
-                                <span className="text-red-500">
-                                    {errors.confirmPassword.message}
-                                </span>
-                            ) : "Re-enter your password"}
-                        </label>
-                        <div className="relative flex items-center">
-                            <input
-                                type={isReenterPasswordVisible ? "text" : "password"}
-                                className={`rounded border ${errors.confirmPassword ? "border-red-500" : "border-black"
-                                    } p-3 pr-10`}
-                                id="confirm_password"
-                                {...formRegister("confirmPassword", {
-                                    required: "Please re-enter your password",
-                                    minLength: {
-                                        value: 8,
-                                        message: "Password must be 8 characters"
-                                    },
-                                    validate: (value) =>
-                                        value === password || "Passwords do not match"
-                                })}
-                                aria-invalid={errors.confirmPassword || password ? "true" : "false"}
-                            />
-                            {isReenterPasswordVisible ? (
-                                <button
-                                    onClick={onClickToggleReenterPasswordVisibility}
-                                    className="absolute right-0 mr-2 hover:text-primary-secondary"
-                                >
-                                    <VisibilityOffIcon />
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={onClickToggleReenterPasswordVisibility}
-                                    className="absolute right-0 mr-2 hover:text-primary-secondary"
-                                >
-                                    <VisibilityIcon />
-                                </button>
-                            )}
-                        </div>
-                    </div>
+                <form
+                    onSubmit={handleSubmit(handleFormSubmit)}
+                    className="grid gap-4"
+                >
+                    <Input
+                        label="Name"
+                        type="text"
+                        register={formRegister}
+                        registerName="name"
+                        registerRequired={{
+                            required: "Name is required",
+                            minLength: {
+                                value: 2,
+                                message: "At least 2 characters long",
+                            },
+                            maxLength: {
+                                value: 50,
+                                message: "Name should not exceed 50 characters",
+                            },
+                        }}
+                        error={errors.name?.message}
+                    />
+                    <Input
+                        label="Email"
+                        type="email"
+                        register={formRegister}
+                        registerName="email"
+                        registerRequired={{
+                            required: "Email is required",
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                message: "Invalid email address",
+                            },
+                        }}
+                        error={errors.email?.message}
+                    />
+                    <Input
+                        label="Password"
+                        type="password"
+                        register={formRegister}
+                        registerName="password"
+                        registerRequired={{
+                            required: "Password is required",
+                            minLength: {
+                                value: 8,
+                                message: "Password must be 8 characters",
+                            },
+                        }}
+                        error={errors.password?.message}
+                        isPasswordVisible={isPasswordVisible}
+                        setIsPasswordVisible={setIsPasswordVisible}
+                    />
+                    <Input
+                        label="Re-enter your password"
+                        type="password"
+                        register={formRegister}
+                        registerName="confirmPassword"
+                        registerRequired={{
+                            required: "Please re-enter your password",
+                            minLength: {
+                                value: 8,
+                                message: "Password must be 8 characters",
+                            },
+                            validate: (value) =>
+                                value === password || "Passwords do not match",
+                        }}
+                        error={errors.confirmPassword?.message}
+                        isPasswordVisible={isReenterPasswordVisible}
+                        setIsPasswordVisible={setIsReenterPasswordVisible}
+                    />
 
                     <button
                         type="submit"
-                        className="w-full rounded bg-primary-black p-3 text-white"
+                        className="mt-4 w-full rounded bg-primary-black p-3 text-white"
                     >
                         Continue
                     </button>
