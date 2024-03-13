73591b1d13e8484267908b8df8e18f9ae0bb5d2e
Neo <lordneobarnachea@gmail.com>
2024-03-13 18:27:54 +0800

    add response templates

---
+++ src/components/FormResponseComponent/DropdownResponse.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DropdownResponse() {
+    return (
+        <div className="my-8 grid min-h-48 w-full grid-cols-1 gap-4 bg-[#444654] p-4 shadow-md">
+            DropdownResponse
+        </div>
+    )
+}
