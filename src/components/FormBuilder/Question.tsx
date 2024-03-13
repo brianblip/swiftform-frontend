468c45bed963f2a6db22253328e1fb27cb64ad4c
Sly Villafania <villafania.sly.lau@gmail.com>
2024-03-13 23:08:51 +0800

    refactor first TextField component to Input Component

--- src/components/FormBuilder/Question.tsx
+++ src/components/FormBuilder/Question.tsx
@@ -68,10 +68,22 @@ export default function QuestionComponent({
             key={question.id}
             className="relative grid gap-4 rounded border border-white/25 p-4 shadow-md"
         >
-            <div className="mb-4 mr-4">
             {/* <h1>Question ID: {question.id}</h1> */}
-                <h1>Question Order: {question.order}</h1>
-                <TextField
+            <h1>Question Order: {question.order}</h1>
+            <Input
+                label="Question Prompt:"
+                type="text"
+                className="w-full rounded bg-primary-secondary px-3 py-2 text-white focus:bg-primary-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                defaultValue={question.prompt}
+                onChange={(e) =>
+                    handleUpdateQuestion(
+                        question.id,
+                        question.type,
+                        e.target.value,
+                    )
+                }
+            />
+            {/* <TextField
                     fullWidth
                     label="Question Prompt"
                     variant="filled"
@@ -84,86 +96,58 @@ export default function QuestionComponent({
                         )
                     }
                 /> */}
-            <div className="grid gap-2">
-                <Input
-                    label="Question Type:"
-                    type="select"
-                    className="bg-primary-secondary p-2 text-white"
-                    defaultValue={question.type}
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                <TextField
+                    fullWidth
+                    select
+                    label="Question Type"
+                    value={question.type}
+                    onChange={(e) =>
                         handleUpdateQuestion(
                             question.id,
                             e.target.value as QuestionType,
                             question.prompt,
                         )
                     }
+                    variant="filled"
+                    className="mt-2"
                 >
-                    {questionTypes.map((questionType) => (
-                        <option
-                            className={`w-full font-sans`}
-                            key={questionType}
-                            value={questionType}
-                        >
-                            {questionType}
-                        </option>
+                    {Object.values(QuestionType).map((type) => (
+                        <MenuItem key={type} value={type}>
+                            {type}
+                        </MenuItem>
                     ))}
-                </Input>
-                {/* <TextField
-                        fullWidth
-                        select
-                        label="Question Type"
-                        value={question.type}
-                        onChange={(e) =>
-                            handleUpdateQuestion(
-                                question.id,
-                                e.target.value as QuestionType,
-                                question.prompt,
-                            )
-                        }
-                        variant="filled"
-                        className="mt-2"
-                    >
-                        {Object.values(QuestionType).map((type) => (
-                            <MenuItem key={type} value={type}>
-                                {type}
-                            </MenuItem>
-                        ))}
-                    </TextField> */}
-                {["multiple_choice", "checkbox", "dropdown"].includes(
-                question.type,
-            ) && (
-                    <div className="grid gap-4 rounded bg-primary-secondary p-4 shadow-2xl">
-                        {/* <h2>Choices:</h2> */}
-                        {sortedChoices.map((choice) => (
-                            <ChoiceComponent
-                                key={choice.id}
-                                choice={choice}
-                                handleUpdateChoice={handleUpdateChoice}
-                                handleDeleteChoice={handleDeleteChoice}
-                            />
-                        ))}
-                        <div className="flex items-center gap-3">
-                            <Input
-                                label="New Choice:"
-                                type="text"
-                                value={newChoiceText}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                    setNewChoiceText(e.target.value)
-                                }
-                            />
-                            {/* <TextField
-                                label="New Choice"
-                                variant="filled"
-                                value={newChoiceText}
-                                onChange={(e) => setNewChoiceText(e.target.value)}
-                            /> */}
-                            <button
-                                onClick={handleCreateChoice}
-                                className="rounded bg-lime-700 p-1 hover:bg-lime-700/75"
-                            >
-                                <AddIcon />
-                            </button>
-                        </div>
+                </TextField>
+            </div>
+            {[
+                QuestionType.MULTIPLE_CHOICE,
+                QuestionType.CHECKBOX,
+                QuestionType.DROPDOWN,
+            ].includes(question.type) && (
+                <div className="flex flex-col items-start">
+                    <h2>Choices:</h2>
+                    {sortedChoices.map((choice) => (
+                        <ChoiceComponent
+                            key={choice.id}
+                            choice={choice}
+                            handleUpdateChoice={handleUpdateChoice}
+                            handleDeleteChoice={handleDeleteChoice}
+                        />
+                    ))}
+                    <div className="mt-2 flex items-center">
+                        <TextField
+                            label="New Choice"
+                            variant="filled"
+                            value={newChoiceText}
+                            onChange={(e) => setNewChoiceText(e.target.value)}
+                        />
+                        <Button
+                            onClick={handleCreateChoice}
+                            variant="contained"
+                            color="primary"
+                            className="ml-2"
+                        >
+                            Add Choice
+                        </Button>
                     </div>
                 )}
             </div>
