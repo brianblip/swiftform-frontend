563e5b38e8fc41e6325241d29ed15726b205cbc9
Neo <lordneobarnachea@gmail.com>
2024-03-13 21:46:41 +0800

    add useState and function for toggling

--- src/components/FormResponseComponent/FormResponseHeader.tsx
+++ src/components/FormResponseComponent/FormResponseHeader.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ToggleButton from '../ui/ToggleButton'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 export default function FormResponseHeader() {
+  const [isChecked, setIsChecked] = useState(false);
+
+    const handleToggle = () => {
+        setIsChecked(!isChecked);
+    };
   return (
     <div className="my-8 grid min-h-48 w-full grid-cols-1 gap-4 rounded-md bg-[#444654] p-4 shadow-md">
       <div className="flex justify-between">
@@ -12,8 +17,8 @@ export default function FormResponseHeader() {
         </div>
       </div>
       <div className="flex items-center justify-end">
-        <p id="responseStatus" className="mr-2">Accepting responses</p>
-        <ToggleButton />
+        <p id="responseStatus" className="mr-2">{isChecked ? 'Accepting responses' : 'Not accepting responses'}</p>
+        <ToggleButton isChecked={isChecked} handleToggle={handleToggle}/>
       </div>
       <div className="flex justify-around">
         <button className='w-full font-semibold active:text-black'>Summary</button>
