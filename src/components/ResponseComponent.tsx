819be5dee0854b44735efb669c63ca30f0c6c1cf
Neo <lordneobarnachea@gmail.com>
2024-03-13 18:33:12 +0800

    Add response components

--- src/components/ResponseComponent.tsx
+++ src/components/ResponseComponent.tsx
@@ -1,10 +1,13 @@
 // ResponseComponent.tsx
 import React from "react";
-
+import TexFieldResponse from "./FormResponseComponent/TexFieldResponse";
+import FormResponseHeader from "./FormResponseComponent/FormResponseHeader";
 export default function ResponseComponent() {
     return (
-        <div>
-            <h2>Response Section</h2>
+        <div className="flex w-full flex-col items-center gap-y-4">
+            <FormResponseHeader />
+            {/* TODO Loop response instead of hardcoding */}
+            <TexFieldResponse />
         </div>
     );
 }
