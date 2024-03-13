50cbdef604f54f0369650b7fcc8ec22311796c1f
Neo <lordneobarnachea@gmail.com>
2024-03-13 21:38:43 +0800

    fix toggleButton

--- src/components/ui/ToggleButton.tsx
+++ src/components/ui/ToggleButton.tsx
@@ -1,13 +1,26 @@
-//TODO: Toggle button still not working
-const ToggleButton = () => {
+import React, { useState } from 'react';
+interface ToggleButtonProps {
+    isChecked: boolean;
+    handleToggle: () => void;
+
+}
+
+const ToggleButton: React.FC<ToggleButtonProps> = ({ isChecked, handleToggle }) => {
     return (
         <label htmlFor="toggleButton" className="flex cursor-pointer items-center">
             <div className="relative">
-                <input type="checkbox" id="toggleButton" className="sr-only" />
-                <div className="block h-6 w-10 rounded-full bg-gray-600"></div>
-                <div className="dot absolute left-1 top-1 size-4 rounded-full bg-white transition"></div>
+                <input
+                    type="checkbox"
+                    id="toggleButton"
+                    className="sr-only"
+                    checked={isChecked}
+                    onChange={handleToggle}
+                />
+                <div className={`block h-6 w-10 rounded-full ${isChecked ? 'bg-red-500' : 'bg-gray-600'}`}></div>
+                <div className={`dot absolute top-1 size-4 rounded-full bg-white transition`} style={{ left: isChecked ? 'calc(100% - 1.5rem)' : '0.25rem' }}></div>
             </div>
         </label>
     )
 }
+
 export default ToggleButton;
