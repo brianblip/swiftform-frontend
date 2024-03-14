ee361929af50bb11a90490f4dbc1c21560f46d67
Renzo Visperas <renzoviperas07@gmail.com>
2024-03-14 12:18:54 +0800

    add answers to response

--- src/app/(main)/Form/[formId]/shared/page.tsx
+++ src/app/(main)/Form/[formId]/shared/page.tsx
@@ -11,14 +11,22 @@ import { useState } from "react";
 
 export default function Shared({ params }: { params: { formId: string } }) {
     const { formId } = params;
-    const { data } = useSWR<Form>(`/forms/${formId}`, fetcher);
+    const { data: form } = useSWR<Form>(`/forms/${formId}`, fetcher);
     const [isCreatingRespose, setIsCreatingResponse] = useState(false);
 
-    const { register, handleSubmit } = useForm({});
+    const { register, handleSubmit, reset } = useForm<{
+        sections: {
+            questions: {
+                text: string;
+            }[];
+        }[];
+    }>({});
 
     const handleSubmitResponse = handleSubmit(async (data) => {
         try {
             setIsCreatingResponse(true);
+
+            // create a response to the form first
             const response = await api.post<ApiResponse<Response>>(
                 `/responses`,
                 {
@@ -26,7 +34,32 @@ export default function Shared({ params }: { params: { formId: string } }) {
                 },
             );
 
-            console.log(response.data.data);
+            const newResponse = response.data.data;
+
+            const questions = form?.sections.flatMap((section) =>
+                section.questions.map((question) => question),
+            );
+
+            // get the answers to the questions
+            const answers = data.sections.flatMap((section) => {
+                return section.questions.map((question, questionIndex) => {
+                    return {
+                        question_id: questions?.[questionIndex].id,
+                        response_id: newResponse?.id,
+                        text: question.text,
+                    };
+                });
+            });
+
+            // add the answers to the response
+            await Promise.all(
+                answers.map((answer) => {
+                    return api.post<ApiResponse<Answer>>(`/answers`, answer);
+                }),
+            );
+
+            reset();
+            alert("Response submitted successfully");
         } catch (e) {
             alert("An error occurred");
         } finally {
@@ -37,10 +70,10 @@ export default function Shared({ params }: { params: { formId: string } }) {
     return (
         <form className="container mx-auto" onSubmit={handleSubmitResponse}>
             <div className="mt-24 border border-black p-4">
-                <TextInput defaultValue={data?.name} disabled label="" />
+                <TextInput defaultValue={form?.name} disabled label="" />
 
                 <TextInput
-                    defaultValue={data?.description}
+                    defaultValue={form?.description}
                     disabled
                     label=""
                     className="mt-4"
@@ -49,7 +82,7 @@ export default function Shared({ params }: { params: { formId: string } }) {
 
             {/* sections */}
             <div className="mt-16 flex flex-col gap-4">
-                {data?.sections.map((section, sectionIndex) => (
+                {form?.sections.map((section, sectionIndex) => (
                     <div
                         key={section.id}
                         className="flex flex-col gap-4 border border-black p-4"
@@ -70,7 +103,7 @@ export default function Shared({ params }: { params: { formId: string } }) {
                                                 label={question.prompt}
                                                 key={question.id}
                                                 {...register(
-                                                    `sections.${sectionIndex}.questions.${questionIndex}`,
+                                                    `sections.${sectionIndex}.questions.${questionIndex}.text`,
                                                 )}
                                             />
                                         );
