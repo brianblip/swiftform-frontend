778933e91a525ec1f23fa4d76acae67869f53185
MAKIENAUT <mcrayescoto@gmail.com>
2024-03-16 00:54:59 +0800

    Remove obsolete commented out codes; Correct state variable typo; Add question type conditional for rendering questions;

--- src/app/(main)/Form/[formId]/shared/page.tsx
+++ src/app/(main)/Form/[formId]/shared/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React, { useState } from "react";
 import useSWR from "swr";
 import { fetcher } from "@/utils";
 import { Form, Response, Answer, ApiResponse } from "@@/types";
@@ -7,12 +7,11 @@ import TextInput from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
 import { useForm } from "react-hook-form";
 import api from "@/services/api";
-import { useState } from "react";
 
 export default function Shared({ params }: { params: { formId: string } }) {
     const { formId } = params;
     const { data: form } = useSWR<Form>(`/forms/${formId}`, fetcher);
-    const [isCreatingRespose, setIsCreatingResponse] = useState(false);
+    const [isCreatingResponse, setIsCreatingResponse] = useState(false);
 
     const { register, handleSubmit, reset } = useForm<{
         sections: {
@@ -75,14 +74,6 @@ export default function Shared({ params }: { params: { formId: string } }) {
             <section className="grid gap-2">
                 <h1 className="text-center text-2xl font-bold">{form?.name}</h1>
                 <p className="break-all">{form?.description}</p>
-                {/* <TextInput defaultValue={form?.name} disabled label="" />
-
-                <TextInput
-                    defaultValue={form?.description}
-                    disabled
-                    label=""
-                    className="mt-4"
-                /> */}
             </section>
 
             {/* sections */}
@@ -93,17 +84,12 @@ export default function Shared({ params }: { params: { formId: string } }) {
                         className="flex flex-col gap-4 rounded bg-primary-secondary p-4"
                     >
                         <h1 className="text-lg font-medium">{section.title}</h1>
-                        {/* <TextInput
-                            disabled
-                            defaultValue={section.title}
-                            label=""
-                        /> */}
 
                         {/* questions */}
                         <div className="flex flex-col gap-4">
-                            {section.questions.map(
-                                (question, questionIndex) => {
-                                    if (question.type === "textfield") {
+                            {section.questions.map((question, questionIndex) => {
+                                switch (question.type) {
+                                    case "textfield":
                                         return (
                                             <TextInput
                                                 label={question.prompt}
@@ -113,9 +99,97 @@ export default function Shared({ params }: { params: { formId: string } }) {
                                                 )}
                                             />
                                         );
-                                    }
-                                },
-                            )}
+                                    case "textarea":
+                                        return (
+                                            <textarea
+                                                className="border border-gray-300 rounded-md px-3 py-2"
+                                                key={question.id}
+                                                {...register(
+                                                    `sections.${sectionIndex}.questions.${questionIndex}.text`,
+                                                )}
+                                            />
+                                        );
+                                    case "multiple_choice":
+                                        return (
+                                            <div
+                                                key={question.id}
+                                                className="flex flex-col gap-1"
+                                            >
+                                                {question.choices.map((choice) => (
+                                                    <label
+                                                        key={choice.id}
+                                                        className="flex items-center gap-2"
+                                                    >
+                                                        <input
+                                                            type="radio"
+                                                            value={choice.text}
+                                                            {...register(
+                                                                `sections.${sectionIndex}.questions.${questionIndex}.text`,
+                                                            )}
+                                                        />
+                                                        <span>{choice.text}</span>
+                                                    </label>
+                                                ))}
+                                            </div>
+                                        );
+                                    case "checkbox":
+                                        return (
+                                            <div
+                                                key={question.id}
+                                                className="flex flex-col gap-1"
+                                            >
+                                                {question.choices.map((choice) => (
+                                                    <label
+                                                        key={choice.id}
+                                                        className="flex items-center gap-2"
+                                                    >
+                                                        <input
+                                                            type="checkbox"
+                                                            value={choice.text}
+                                                            {...register(
+                                                                `sections.${sectionIndex}.questions.${questionIndex}.text`,
+                                                            )}
+                                                        />
+                                                        <span>{choice.text}</span>
+                                                    </label>
+                                                ))}
+                                            </div>
+                                        );
+                                    case "dropdown":
+                                        return (
+                                            <select
+                                                key={question.id}
+                                                {...register(
+                                                    `sections.${sectionIndex}.questions.${questionIndex}.text`,
+                                                )}
+                                                className="border border-gray-300 rounded-md px-3 py-2"
+                                            >
+                                                {question.choices.map((choice) => (
+                                                    <option
+                                                        key={choice.id}
+                                                        value={choice.text}
+                                                    >
+                                                        {choice.text}
+                                                    </option>
+                                                ))}
+                                            </select>
+                                        );
+                                    case "date":
+                                        return (
+                                            <input
+                                                type="date"
+                                                key={question.id}
+                                                {...register(
+                                                    `sections.${sectionIndex}.questions.${questionIndex}.text`,
+                                                )}
+                                                className="border border-gray-300 rounded-md px-3 py-2"
+                                            />
+                                        );
+                                    default:
+                                        return null;
+                                        // TODO: Create an input field for Attachment
+                                }
+                            })}
                         </div>
                     </div>
                 ))}
@@ -124,9 +198,9 @@ export default function Shared({ params }: { params: { formId: string } }) {
             <button
                 type="submit"
                 className="mb-8 rounded bg-white px-2 py-3 text-black hover:bg-white/75"
-                disabled={isCreatingRespose}
+                disabled={isCreatingResponse}
             >
-                {isCreatingRespose ? "Submitting..." : "Submit"}
+                {isCreatingResponse ? "Submitting..." : "Submit"}
             </button>
         </form>
     );
