b2bc88b5fc17bb07b485b59afba8d4319f688738
Neo <lordneobarnachea@gmail.com>
2024-03-15 14:33:47 +0800

    delete unecesassry components

--- src/components/FormResponseComponent/TextAreaResponse.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TextAreaResponse() {
-    return (
-        <div className="my-8 grid min-h-48 w-full grid-cols-1 gap-4 bg-[#444654] p-4 shadow-md">
-            TextAreaResponse
-        </div>
-    )
-}
