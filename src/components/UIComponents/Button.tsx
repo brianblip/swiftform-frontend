83894a0e134426ec8e1ba7b9fa35626843bfcc66
Sly Villafania <villafania.sly.lau@gmail.com>
2024-03-16 17:50:16 +0800

    add the variants object
    add the variants object that takes a variant object with values that would add classNames on the base className (the one before this object)

--- src/components/UIComponents/Button.tsx
+++ src/components/UIComponents/Button.tsx
@@ -9,4 +9,16 @@ export default function Button(props: ButtonProps) {
     return <button {...props} />;
 }
 
-const buttonVariants = cva("w-full rounded px-5 py-3 transition");
+const buttonVariants = cva("w-full rounded px-5 py-3 transition", {
+    variants: {
+        variant: {
+            primary:
+                "bg-primary-secondary text-white hover:bg-primary-white/25 disabled:bg-primary-black disabled:text-primary-neutral",
+            secondary:
+                "bg-white text-primary-black hover:bg-white/75 disabled:bg-white/50 disabled:text-primary-neutral",
+            modal: "bg-primary-neutral hover:bg-primary-black/50 disabled:bg-primary-black disabled:text-primary-neutral",
+            exit: "absolute right-0 top-0 w-fit p-0 hover:bg-error",
+            navbar: "flex gap-2 rounded p-2 hover:bg-primary-secondary",
+        },
+    },
+});
