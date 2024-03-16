2701c928f8f905c67cb737f3aac450a5e5002be0
Renzo Visperas <renzoviperas07@gmail.com>
2024-03-16 13:01:20 +0800

    add api error handler

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,16 @@
 import api from "@/services/api";
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
 
 export const fetcher = async (url: string) => {
     const { data } = await api.get(url);
     return data.data;
+};
+
+export const handleApiError = (e: unknown) => {
+    if (e instanceof AxiosError) {
+        toast.error(e.response?.data.message || e.message);
+    } else {
+        toast.error("An unkown error occurred. Please try again.");
+    }
 };
