ed706f42157a34e79f7596f803d38a7780ded39c
Sly Villafania <villafania.sly.lau@gmail.com>
2024-03-17 14:19:26 +0800

    update ProfileButton to conform to new Button component update

--- src/components/UIComponents/ProfileButton.tsx
+++ src/components/UIComponents/ProfileButton.tsx
@@ -36,14 +36,15 @@ const ProfileButton = () => {
         <div ref={profileRef} className="relative md:flex md:items-center">
             <Button
                 variant="navbar"
+                size="sm"
                 onClick={onClickToggleProfile}
-                className={`${isProfileOpen ? "bg-primary-secondary" : ""}`}
+                className={`${isProfileOpen ? "bg-primary-secondary" : ""} p-2`}
             >
                 <PermIdentityOutlinedIcon />
                 <p>My Account</p>
             </Button>
             <div
-                className={`absolute bottom-full -z-10 mb-2 flex w-full origin-bottom flex-col items-start rounded bg-primary-secondary p-2 shadow-2xl transition md:bottom-auto md:left-[calc(100%+theme(spacing.4))] md:-z-50 md:mb-0 md:flex md:max-h-40 md:overflow-y-auto ${isProfileOpen ? "md:translate-x-0" : "scale-0 md:translate-x-[-105%] md:scale-100"}`}
+                className={`absolute bottom-full mb-2 flex w-full origin-bottom flex-col items-start rounded bg-primary-secondary p-2 shadow-2xl transition md:bottom-auto md:left-[calc(100%+theme(spacing.4))] md:-z-50 md:mb-0 md:flex md:max-h-40 md:overflow-y-auto ${isProfileOpen ? "md:translate-x-0" : "scale-0 md:translate-x-[-105%] md:scale-100"}`}
             >
                 <Link
                     href="/profile"
