13111ed5bb7992628bd9888e9aa289b847d2ca06
Neo <lordneobarnachea@gmail.com>
2024-03-18 15:26:43 +0800

    remove horizontal sroll bar

--- src/components/UIComponents/Main.tsx
+++ src/components/UIComponents/Main.tsx
@@ -19,7 +19,7 @@ const mainVariants = cva("flex h-dvh w-dvw items-center justify-center", {
     variants: {
         variant: {
             primary: "h-[calc(100dvh-57.0667px)] p-4 sm:p-8 md:h-dvh xl:px-16",
-            form: "h-[calc(100vh-57.0667px)] flex-col justify-start gap-8 overflow-scroll px-4 py-10 sm:px-8 md:h-dvh md:py-16 xl:p-16",
+            form: "h-[calc(100vh-57.0667px)] flex-col justify-start gap-8 overflow-x-hidden px-4 py-10 sm:px-8 md:h-dvh md:py-16 xl:p-16",
             auth: "p-4",
         },
     },
