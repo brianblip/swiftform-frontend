ce5627299387edb90cd2d85626687a2a20e315ac
Sly Villafania <villafania.sly.lau@gmail.com>
2024-03-20 00:25:07 +0800

    bring mainVariants inside Main function in Main component

--- src/components/UIComponents/Main.tsx
+++ src/components/UIComponents/Main.tsx
@@ -10,20 +10,21 @@ type MainProps = React.DetailedHTMLProps<
 };
 
 export default function Main({ className, variant, ...props }: MainProps) {
+    const mainVariants = cva("flex h-dvh w-dvw items-center justify-center", {
+        variants: {
+            variant: {
+                primary:
+                    "h-[calc(100dvh-57.0667px)] p-4 sm:p-8 md:h-dvh xl:px-16",
+                form: "h-[calc(100vh-57.0667px)] flex-col justify-start gap-8 overflow-x-hidden px-4 py-10 sm:px-8 md:h-dvh md:py-16 xl:p-16",
+                auth: "p-4",
+            },
+        },
+        defaultVariants: {
+            variant: "primary",
+        },
+    });
+
     return (
         <main {...props} className={cn(mainVariants({ variant }), className)} />
     );
 }
-
-const mainVariants = cva("flex h-dvh w-dvw items-center justify-center", {
-    variants: {
-        variant: {
-            primary: "h-[calc(100dvh-57.0667px)] p-4 sm:p-8 md:h-dvh xl:px-16",
-            form: "h-[calc(100vh-57.0667px)] flex-col justify-start gap-8 overflow-x-hidden px-4 py-10 sm:px-8 md:h-dvh md:py-16 xl:p-16",
-            auth: "p-4",
-        },
-    },
-    defaultVariants: {
-        variant: "primary",
-    },
-});
