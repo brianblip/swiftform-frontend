0d5c6dfaae7950584cf1d9ec57ce2664f4d43114
Neo <lordneobarnachea@gmail.com>
2024-03-19 20:25:33 +0800

    add !

--- src/app/(main)/Form/[formId]/page.tsx
+++ src/app/(main)/Form/[formId]/page.tsx
@@ -87,7 +87,7 @@ export default function FormPage({ params }: { params: { formId: number } }) {
                         </button>
                         <button
                             onClick={() => setIsQuestionSectionOpen(false)}
-                            className={`border-b-2 hover:text-white/75 ${isQuestionSectionOpen ? "border-b-primary-white font-bold text-white hover:text-white" : "border-b-transparent"}`}
+                            className={`border-b-2 hover:text-white/75 ${!isQuestionSectionOpen ? "border-b-primary-white font-bold text-white hover:text-white" : "border-b-transparent"}`}
                         >
                             Response
                         </button>
