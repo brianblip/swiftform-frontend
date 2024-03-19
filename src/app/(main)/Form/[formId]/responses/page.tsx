992bce3db4e59bf7b42dcecb2f0eedf824eb9f27
Brian Prado <pradojohnbrian@gmail.com>
2024-03-19 15:21:35 +0800

    show responses in response tab

--- src/app/(main)/Form/[formId]/responses/page.tsx
+++ src/app/(main)/Form/[formId]/responses/page.tsx
@@ -2,8 +2,35 @@
 
 import useSWR from "swr";
 import { fetcher } from "@/utils";
-import { Response, Form } from "@@/types";
+import { Response, Form, User } from "@@/types";
 import Link from "next/link";
+
+interface ResponseItemProps {
+    response: Response;
+    formId: string;
+}
+
+function ResponseItem({ response, formId }: ResponseItemProps) {
+    const { data: userData } = useSWR<User>(
+        `/users/me?user_id=${response.user_id}`,
+        fetcher,
+    );
+
+    if (!userData) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <li key={response.id} className="py-2">
+            <Link
+                href={`/Form/${formId}/responses/${response.id}`}
+                className="text-blue-500 hover:text-blue-700"
+            >
+                {userData.name} - {response.created_at}
+            </Link>
+        </li>
+    );
+}
 
 export default function ResponseList({
     params,
@@ -23,18 +50,16 @@ export default function ResponseList({
     }
 
     return (
-        <div className="container mx-auto mt-24">
+        <div className="container mx-auto mt-5">
             <div className="border border-black p-4">
-                <h2>{formData.name}</h2>
-                <ul>
+                <h2 className="text-xl font-bold">{formData.name}</h2>
+                <ul className="mt-4">
                     {responsesData.map((response) => (
-                        <li key={response.id}>
-                            <Link
-                                href={`/Form/${formId}/responses/${response.id}`}
-                            >
-                                Response #{response.id}
-                            </Link>
-                        </li>
+                        <ResponseItem
+                            key={response.id}
+                            response={response}
+                            formId={formId}
+                        />
                     ))}
                 </ul>
             </div>
