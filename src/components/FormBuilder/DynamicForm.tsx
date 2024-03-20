cf377b8958d0ea9d2b911557bfa9544219ceb3ab
MAKIENAUT <mcrayescoto@gmail.com>
2024-03-21 00:28:29 +0800

    Add "Share form to respondent" button that copies the current url into their clipboard; Add Sort, Mobility, And Duplicate Feature;

--- src/components/FormBuilder/DynamicForm.tsx
+++ src/components/FormBuilder/DynamicForm.tsx
@@ -1,16 +1,15 @@
-// DynamicForm.tsx
 "use client";
 
 import { mutate } from "swr";
-import { Form, Section } from "@@/types";
-import React, { useState, useEffect } from "react";
-import SectionComponent from "./Section";
-import useForm, { FormState } from "@/contexts/forms";
-import Input from "../UIComponents/Input";
 import { toast } from "react-toastify";
-import { Axios, AxiosError } from "axios";
+import useForm from "@/contexts/forms";
 import { handleApiError } from "@/utils";
+import SectionComponent from "./Section";
+import Input from "../UIComponents/Input";
 import Button from "../UIComponents/Button";
+import { usePathname } from "next/navigation";
+import { Form, Section, FormState } from "@@/types";
+import React, { useState, useEffect, useRef } from "react";
 
 type DynamicFormProps = {
     form: Form;
@@ -18,11 +17,24 @@ type DynamicFormProps = {
 };
 
 export default function DynamicForm({ form, updateForm }: DynamicFormProps) {
+    const pathname = usePathname();
+    const formRef = useRef<HTMLFormElement>(null);
     const [description, setDescription] = useState<string>(
         form.description || "",
     );
-    const { createSection, updateSection, deleteSection, createQuestion, createChoice } = useForm();
+    const {
+        createSection,
+        updateSection,
+        deleteSection,
+        createQuestion,
+        createChoice,
+    } = useForm();
     const [isUpdatingForm, setIsUpdatingForm] = useState(false);
+    const [sortedSections, setSortedSections] = useState<Section[]>([]);
+
+    useEffect(() => {
+        setSortedSections([...form.sections].sort((a, b) => a.order - b.order));
+    }, [form.sections]);
 
     useEffect(() => {
         setDescription(form.description || "");
@@ -30,7 +42,11 @@ export default function DynamicForm({ form, updateForm }: DynamicFormProps) {
 
     const handleCreateSection = async () => {
         try {
-            await createSection("New Section", form.id);
+            await createSection(
+                "New Section",
+                form.id,
+                form.sections.length + 1,
+            );
             mutate("/forms");
         } catch (error) {
             console.error("Error creating section:", error);
@@ -49,34 +65,110 @@ export default function DynamicForm({ form, updateForm }: DynamicFormProps) {
     const handleDuplicateSection = async (sectionId: number) => {
         try {
             const sectionToDuplicate = form.sections.find(
-                (section) => section.id === sectionId
+                (section) => section.id === sectionId,
             );
-    
+
             if (!sectionToDuplicate) {
                 console.error(`Section with ID ${sectionId} not found`);
                 return;
             }
-    
-            const newSection = await createSection(sectionToDuplicate.title, form.id);
-    
+
+            const newSection = await createSection(
+                sectionToDuplicate.title,
+                form.id,
+                form.sections.length + 1,
+            );
+
             for (const question of sectionToDuplicate.questions) {
                 const newQuestion = await createQuestion(
                     question.type,
                     question.prompt,
                     newSection.id,
                     question.order,
-                    question.is_required
+                    question.is_required,
                 );
-    
+
                 for (const choice of question.choices) {
-                    await createChoice(choice.text, newQuestion.id, choice.order);
+                    await createChoice(
+                        choice.text,
+                        newQuestion.id,
+                        choice.order,
+                    );
                 }
             }
-    
+
             mutate("/forms");
         } catch (error) {
             console.error("Error duplicating section:", error);
         }
+    };
+
+    const updateSortedSections = (updatedSections: Section[]) => {
+        setSortedSections(updatedSections.sort((a, b) => a.order - b.order));
+    };
+
+    const moveSectionUp = (sectionId: number) => {
+        const updatedSections = [...sortedSections];
+        const sectionIndex = updatedSections.findIndex(
+            (q) => q.id === sectionId,
+        );
+
+        if (sectionIndex > 0) {
+            const prevSection = updatedSections[sectionIndex - 1];
+            const currentSection = updatedSections[sectionIndex];
+
+            prevSection.order = currentSection.order;
+            currentSection.order = prevSection.order - 1;
+
+            updateMultipleSections(updatedSections, updateSortedSections);
+            scrollToSection(sectionId);
+        }
+    };
+
+    const moveSectionDown = (sectionId: number) => {
+        const updatedSections = [...sortedSections];
+        const sectionIndex = updatedSections.findIndex(
+            (q) => q.id === sectionId,
+        );
+
+        if (sectionIndex < updatedSections.length - 1) {
+            const nextSection = updatedSections[sectionIndex + 1];
+            const currentSection = updatedSections[sectionIndex];
+
+            nextSection.order = currentSection.order;
+            currentSection.order = nextSection.order + 1;
+
+            updateMultipleSections(updatedSections, updateSortedSections);
+            scrollToSection(sectionId);
+        }
+    };
+
+    const scrollToSection = (sectionId: number) => {
+        const sectionElement = document.getElementById(`section_${sectionId}`);
+        if (sectionElement && formRef.current) {
+            const { y } = sectionElement.getBoundingClientRect();
+            const scrollOffset =
+                y -
+                formRef.current.offsetHeight / 2 +
+                sectionElement.offsetHeight / 2;
+            window.scrollTo({
+                top: window.pageYOffset + scrollOffset,
+                behavior: "smooth",
+            });
+        }
+    };
+
+    const updateMultipleSections = async (
+        updatedSections: Section[],
+        updateSortedSections: (updatedSections: Section[]) => void,
+    ) => {
+        const promises = updatedSections.map((section) => {
+            return updateSection(section.id, section.title, section.order);
+        });
+
+        await Promise.all(promises);
+        mutate("/forms");
+        updateSortedSections(updatedSections.sort((a, b) => a.order - b.order));
     };
 
     const handleSubmit = async (event: React.FormEvent) => {
@@ -93,9 +185,16 @@ export default function DynamicForm({ form, updateForm }: DynamicFormProps) {
         }
     };
 
+    const handleShareForm = () => {
+        const shareLink = `https://swiftform.boomtech.co${pathname}/shared`;
+        navigator.clipboard.writeText(shareLink);
+        toast.success("Copied form link to clipboard!");
+    };
+
     return (
         <form
             onSubmit={handleSubmit}
+            ref={formRef}
             className="grid w-full gap-4 sm:w-11/12 lg:w-9/12 xl:w-[660px]"
         >
             <Input
@@ -107,18 +206,19 @@ export default function DynamicForm({ form, updateForm }: DynamicFormProps) {
                     setDescription(e.target.value)
                 }
             />
-            {Array.isArray(form.sections) && form.sections.length > 0 ? (
-                form.sections
-                    .sort((a, b) => a.id - b.id)
-                    .map((section: Section) => (
-                        <SectionComponent
-                            key={section.id}
-                            section={section}
-                            updateSection={updateSection}
-                            handleDeleteSection={handleDeleteSection}
-                            handleDuplicateSection={handleDuplicateSection}
-                        />
-                    ))
+            {sortedSections.length ? (
+                sortedSections.map((section: Section) => (
+                    <SectionComponent
+                        key={section.id}
+                        section={section}
+                        sortedSections={sortedSections}
+                        updateSection={updateSection}
+                        moveSectionUp={moveSectionUp}
+                        moveSectionDown={moveSectionDown}
+                        handleDeleteSection={handleDeleteSection}
+                        handleDuplicateSection={handleDuplicateSection}
+                    />
+                ))
             ) : (
                 <p>No sections found.</p>
             )}
@@ -133,6 +233,10 @@ export default function DynamicForm({ form, updateForm }: DynamicFormProps) {
 
             <Button type="submit" variant="secondary" className="mb-10">
                 {isUpdatingForm ? "Saving..." : "Save Changes"}
+            </Button>
+
+            <Button type="button" className="mb-4" onClick={handleShareForm}>
+                Share Form to Respondents
             </Button>
         </form>
     );
