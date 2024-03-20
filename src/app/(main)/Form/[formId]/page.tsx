ac3bf7e195a48b70aafeb7b75abfbf5ca9c43b2b
Sly Villafania <villafania.sly.lau@gmail.com>
2024-03-20 22:13:21 +0800

    add better css conditional to main form page

--- src/app/(main)/Form/[formId]/page.tsx
+++ src/app/(main)/Form/[formId]/page.tsx
@@ -82,14 +82,14 @@ export default function FormPage({ params }: { params: { formId: number } }) {
                         <button
                             type="button"
                             onClick={() => setIsQuestionSectionOpen(true)}
-                            className={`border-b-2 hover:text-white/75 ${isQuestionSectionOpen ? "border-b-primary-white font-bold text-white hover:text-white" : "border-b-transparent"}`}
+                            className={`border-b-2 ${isQuestionSectionOpen ? "border-b-primary-white font-bold text-white hover:text-white" : "border-b-transparent hover:text-white/75"}`}
                         >
                             Question
                         </button>
                         <button
                             type="button"
                             onClick={() => setIsQuestionSectionOpen(false)}
-                            className={`border-b-2 hover:text-white/75 ${!isQuestionSectionOpen ? "border-b-primary-white font-bold text-white hover:text-white" : "border-b-transparent"}`}
+                            className={`border-b-2 ${!isQuestionSectionOpen ? "border-b-primary-white font-bold text-white hover:text-white" : "border-b-transparent hover:text-white/75"}`}
                         >
                             Response
                         </button>
