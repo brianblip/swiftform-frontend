31147ada4047a3fb928527bb6c5773a6b8e19eb4
Sly Villafania <villafania.sly.lau@gmail.com>
2024-03-21 10:57:28 +0800

    update Main component primary styling

--- src/components/UIComponents/Main.tsx
+++ src/components/UIComponents/Main.tsx
@@ -14,7 +14,7 @@ export default function Main({ className, variant, ...props }: MainProps) {
         variants: {
             variant: {
                 primary:
-                    "h-[calc(100dvh-57.0667px)] p-4 sm:p-8 md:h-dvh xl:px-16",
+                    "h-[calc(100dvh-57.0667px)] px-4 py-10 sm:px-8 md:h-dvh md:py-16 xl:p-16",
                 form: "h-[calc(100vh-57.0667px)] flex-col justify-start gap-8 overflow-x-hidden px-4 py-10 sm:px-8 md:h-dvh md:py-16 xl:p-16",
                 auth: "p-4",
             },
